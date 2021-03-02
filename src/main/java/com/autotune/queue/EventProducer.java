20a304496f72e6cac694273c10ae2e4d05ad991d
Bipin Kumar <bipinmaurya@gmail.com>
2021-03-02 13:21:09 +0530

    Adding queue implementaion core classess

---
+++ src/main/java/com/autotune/queue/EventProducer.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Copyright (c) 2020, 2021 Red Hat, IBM Corporation and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package com.autotune.queue;
+
+import java.util.concurrent.Callable;
+
+public class EventProducer implements Callable {
+	
+	private final AutotuneQueue queue;
+	private final AutotuneDTO inputDTO;
+	
+	public EventProducer(AutotuneQueue queue, AutotuneDTO inputData) {
+		this.queue = queue;
+		this.inputDTO = inputData;
+	}
+
+	
+	@Override
+	public Boolean call() {
+			boolean wasAdded=false;
+			try {
+				wasAdded = queue.send(inputDTO);
+				
+					
+			} catch (InterruptedException e) {
+				
+				System.out.println("Adding Thread was interrupted");
+				
+			} finally {
+				
+				System.out.println("Added object to queue of Id:" + inputDTO.getId() + " wasAdded: " + wasAdded);
+			}
+			
+			return wasAdded;
+		}
+}
---
+++ src/main/java/com/autotune/queue/RecMgrQueue.java
@@ -0,0 +1,72 @@
+/*******************************************************************************
+ * Copyright (c) 2020, 2021 Red Hat, IBM Corporation and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package com.autotune.queue;
+
+import java.io.Serializable;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import com.autotune.em.utils.EMUtils.QueueName;
+
+public class RecMgrQueue implements AutotuneQueue, Serializable {
+	private static final long serialVersionUID = -6045964856984857449L;
+	private final int INITIAL_CAPACITY = 50;
+	private final BlockingQueue<AutotuneDTO> queue;
+	private final String name;
+	private static RecMgrQueue instance;
+
+	private RecMgrQueue()
+	{
+		this.name = QueueName.RECMGRQUEUE.name();
+		queue = new LinkedBlockingQueue<AutotuneDTO>(INITIAL_CAPACITY);
+	}
+
+	public static RecMgrQueue getInstance()
+	{
+		if (instance == null)
+		{
+			synchronized(RecMgrQueue.class)
+			{
+				if (instance == null)
+				{
+					instance = new RecMgrQueue();
+				}
+			}            
+		}
+
+		return instance;
+	} 
+	//
+	private Object readResolve() {
+		return getInstance();
+	}
+	
+	@Override
+	public boolean send(AutotuneDTO data) throws InterruptedException {
+		return queue.offer(data);
+	}
+	
+	@Override
+	public AutotuneDTO get() throws InterruptedException {
+		return queue.take();
+	}
+	
+	@Override
+	public String getName() {
+		return this.name;
+	}
+		
+}
