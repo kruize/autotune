62d5420f2b094cf48df49fc9e8be8cefdbedf622
Rounak Agarwal <rounakr.ag73@gmail.com>
2021-04-12 16:51:36 +0530

    Add getter and setter methods

--- hyperparameter_tuning/service.py
+++ hyperparameter_tuning/service.py
@@ -27,7 +27,11 @@ from tunables import get_all_tunables
 
 from bayes_optuna import optuna_hpo
 
-autotune_object_ids = []
+autotune_object_ids = {}
+
+api_endpoint = "/experiment_trials"
+host_name = "localhost"
+server_port = 8085
 
 
 class HTTPRequestHandler(BaseHTTPRequestHandler):
@@ -39,8 +43,7 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
         self.wfile.write(return_value.encode('utf-8'))
 
     def do_POST(self):
-        # TODO: perform better regex search
-        if re.search('/experiment_trials', self.path):
+        if re.search(api_endpoint + "$", self.path):
             content_type, params = cgi.parse_header(self.headers.get('content-type'))
             if content_type == 'application/json':
                 length = int(self.headers.get('content-length'))
@@ -48,26 +51,24 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
                 json_object = json.loads(str_object)
                 # TODO: validate structure of json_object for each operation
                 if json_object["operation"] == "EXP_TRIAL_GENERATE_NEW":
-                    if json_object["id"] not in autotune_object_ids:
-                        autotune_object_ids.append(json_object["id"])
+                    if json_object["id"] not in autotune_object_ids.keys():
                         get_search_create_study(json_object["id"], json_object["operation"], json_object["url"])
-                        trial_number = optuna_hpo.TrialDetails.trial_number
+                        trial_number = get_trial_number(json_object["id"])
                         self._set_response(200, str(trial_number))
                     else:
                         self._set_response(400, "-1")
                 elif json_object["operation"] == "EXP_TRIAL_GENERATE_SUBSEQUENT":
-                    if json_object["id"] in autotune_object_ids:
+                    if json_object["id"] in autotune_object_ids.keys():
                         get_search_create_study(json_object["id"], json_object["operation"], json_object["url"])
-                        trial_number = optuna_hpo.TrialDetails.trial_number
+                        trial_number = get_trial_number(json_object["id"])
                         self._set_response(200, str(trial_number))
                     else:
                         self._set_response(400, "-1")
                 elif json_object["operation"] == "EXP_TRIAL_RESULT":
-                    if json_object["id"] in autotune_object_ids and json_object["trial_number"] == optuna_hpo.TrialDetails.trial_number:
-                        optuna_hpo.TrialDetails.trial_result = json_object["trial_result"]
-                        optuna_hpo.TrialDetails.result_value_type = json_object["result_value_type"]
-                        optuna_hpo.TrialDetails.result_value = json_object["result_value"]
-                        optuna_hpo.TrialDetails.trial_result_received = 1
+                    if (json_object["id"] in autotune_object_ids.keys() and
+                            json_object["trial_number"] == get_trial_number(json_object["id"])):
+                        set_result(json_object["id"], json_object["trial_result"], json_object["result_value_type"],
+                                   json_object["result_value"])
                         self._set_response(200, "0")
                     else:
                         self._set_response(400, "-1")
@@ -79,12 +80,11 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
             self._set_response(403, "-1")
 
     def do_GET(self):
-        # TODO: perform better regex search
-        if re.search('/experiment_trials', self.path):
+        if re.search(api_endpoint, self.path):
             query = parse_qs(urlparse(self.path).query)
-            if ("id" in query and "trial_number" in query and query["id"][0] in autotune_object_ids and
-                    query["trial_number"][0] == str(optuna_hpo.TrialDetails.trial_number)):
-                data = json.dumps(optuna_hpo.TrialDetails.trial_json_object)
+            if ("id" in query and "trial_number" in query and query["id"][0] in autotune_object_ids.keys() and
+                    query["trial_number"][0] == str(get_trial_number(query["id"][0]))):
+                data = get_trial_json_object(query["id"][0])
                 self._set_response(200, data)
             else:
                 self._set_response(404, "-1")
@@ -96,7 +96,9 @@ def get_search_create_study(id_, operation, url):
     # TODO: validate structure of search_space_json
     search_space_json = get_search_space(id_, url)
     if operation == "EXP_TRIAL_GENERATE_NEW":
-        application_name, direction, hpo_algo_impl, id_, objective_function, tunables, value_type = get_all_tunables(search_space_json)
+        application_name, direction, hpo_algo_impl, id_, objective_function, tunables, value_type = get_all_tunables(
+            search_space_json)
+        autotune_object_ids[id_] = hpo_algo_impl
         if hpo_algo_impl in ("optuna_tpe", "optuna_tpe_multivariate", "optuna_skopt"):
             threading.Thread(
                 target=optuna_hpo.recommend, args=(application_name, direction, hpo_algo_impl, id_, objective_function,
@@ -111,10 +113,27 @@ def get_search_space(id_, url):
     return search_space_json
 
 
-def main():
-    host_name = "localhost"
-    server_port = 8085
+def get_trial_number(id_):
+    if autotune_object_ids[id_] in ("optuna_tpe", "optuna_tpe_multivariate", "optuna_skopt"):
+        trial_number = optuna_hpo.TrialDetails.trial_number
+    return trial_number
 
+
+def get_trial_json_object(id_):
+    if autotune_object_ids[id_] in ("optuna_tpe", "optuna_tpe_multivariate", "optuna_skopt"):
+        trial_json_object = json.dumps(optuna_hpo.TrialDetails.trial_json_object)
+    return trial_json_object
+
+
+def set_result(id_, trial_result, result_value_type, result_value):
+    if autotune_object_ids[id_] in ("optuna_tpe", "optuna_tpe_multivariate", "optuna_skopt"):
+        optuna_hpo.TrialDetails.trial_result = trial_result
+        optuna_hpo.TrialDetails.result_value_type = result_value_type
+        optuna_hpo.TrialDetails.result_value = result_value
+        optuna_hpo.TrialDetails.trial_result_received = 1
+
+
+def main():
     server = HTTPServer((host_name, server_port), HTTPRequestHandler)
     print("Starting server at http://%s:%s" % (host_name, server_port))
     server.serve_forever()
