b0e444442b5205ee9755214828c78b45defe7b0d
bharathappali <bharath.appali@gmail.com>
2021-04-26 10:45:12 +0530

    Adds handler classes ConfigurationReceiverHandler, DeployAppConfigurationHandler
    Signed-off-by: bipinmaurya <bipinmaurya@gmail.com>
    Signed-off-by: bharathappali <bharath.appali@gmail.com>

--- src/main/java/com/autotune/experimentManager/utils/EMUtils.java
+++ src/main/java/com/autotune/experimentManager/utils/EMUtils.java
@@ -32,6 +32,23 @@ public final class EMUtils {
 
     public static final String DEFAULT_EVENT_NAME = "event";
     public static final String DEFAULT_TRANSITION_NAME = "transition";
+    public static String NAMESPACE="default";
+
+    public static String NEW_DEPLOYMENT_NAME_SUFIX="autotune-trial";
+
+    public static String TRIALS ="trials";
+    public static String DEPLOYMENT_NAME_KEY = "deployment_name";
+    public static String UPDATE_CONFIG = "update_config";
+    public static String ID="id";
+    public static String APP_VERSION="app-version";
+    public static String DEPLOYMENT_NAME="deployment_name";
+    public static String TRIAL_NUM= "trial_num";
+    public static String TRIAL_RUN="trial_run";
+    public static String TRIAL_MEASUREMENT_TIME="trial_measurement_time";
+    public static String METRICS="metrics";
+    public static String NAME="name";
+    public static String QUERY="query";
+    public static String DATASOURCE="datasource";
 
     public static String dumpFSMStates(final Set<EMState> states) {
         StringBuilder result = new StringBuilder();
