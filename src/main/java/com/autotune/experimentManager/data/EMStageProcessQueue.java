0e04d37cdfe831b1760d5922e04631d07efc8083
bharathappali <bharath.appali@gmail.com>
2021-08-30 10:58:56 +0530

    Added EM Process Queue handler and Stage Transition
    Signed-off-by: bharathappali <abharath@redhat.com>

---
+++ src/main/java/com/autotune/experimentManager/data/EMStageProcessQueue.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright (c) 2021, 2021 Red Hat, IBM Corporation and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+
+package com.autotune.experimentManager.data;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class EMStageProcessQueue {
+    private static volatile EMStageProcessQueue emStageProcessQueue = null;
+    private volatile ConcurrentLinkedQueue<EMStageTransition> emStageQueue;
+    private volatile ConcurrentLinkedQueue<EMStageScheduledTransition> emStageScheduledQueue;
+
+    private EMStageProcessQueue() {
+        emStageQueue = new ConcurrentLinkedQueue<EMStageTransition>();
+        emStageScheduledQueue = new ConcurrentLinkedQueue<EMStageScheduledTransition>();
+    }
+
+    public static EMStageProcessQueue getStageProcessQueueInstance() {
+        if (null == emStageProcessQueue) {
+            synchronized (EMStageProcessQueue.class) {
+                if (null == emStageProcessQueue) {
+                    emStageProcessQueue = new EMStageProcessQueue();
+                }
+            }
+        }
+        return emStageProcessQueue;
+    }
+
+    public ConcurrentLinkedQueue<EMStageTransition> getQueue() {
+        return emStageQueue;
+    }
+
+    public ConcurrentLinkedQueue<EMStageScheduledTransition> getScheduledQueue() {
+        return emStageScheduledQueue;
+    }
+}
---
+++ src/main/java/com/autotune/experimentManager/utils/EMUtil.java
@@ -0,0 +1,93 @@
+/*******************************************************************************
+ * Copyright (c) 2021, 2021 Red Hat, IBM Corporation and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+
+package com.autotune.experimentManager.utils;
+
+import java.util.UUID;
+
+public class EMUtil {
+    public enum EMExpStages {
+        INIT(0, 1, false, null),
+        CREATE_CONFIG(1, 1, false, EMConstants.TransitionClasses.CREATE_CONFIG),
+        DEPLOY_CONFIG(2, 1, false, EMConstants.TransitionClasses.DEPLOY_CONFIG),
+        INITIATE_TRAIL_RUN_PHASE(3, 1, false, EMConstants.TransitionClasses.INITIATE_TRAIL_RUN_PHASE),
+        INITIAL_LOAD_CHECK(3, 2, true, EMConstants.TransitionClasses.INITIAL_LOAD_CHECK),
+        LOAD_CONSISTENCY_CHECK(3, 3, true, EMConstants.TransitionClasses.LOAD_CONSISTENCY_CHECK),
+        INITIATE_METRICS_COLLECTION_PHASE(4, 1, false, EMConstants.TransitionClasses.INITIATE_METRICS_COLLECTION_PHASE),
+        COLLECT_METRICS(4, 1, true, EMConstants.TransitionClasses.COLLECT_METRICS),
+        CREATE_RESULT_DATA(5, 1, false, EMConstants.TransitionClasses.CREATE_RESULT_DATA),
+        SEND_RESULT_DATA(5, 2, false, EMConstants.TransitionClasses.SEND_RESULT_DATA),
+        CLEAN_OR_ROLLBACK_DEPLOYMENT(6, 1, true, EMConstants.TransitionClasses.CLEAN_OR_ROLLBACK_DEPLOYMENT),
+        EXIT(7, 1, false, null)
+        ;
+
+        private int stage;
+        private int intermediate_stage;
+        private boolean isScheduled;
+        private String className;
+        private static final EMExpStages values[] = values();
+
+        private EMExpStages(final int stage, final int intermediate_stage, final boolean isScheduled, final String className) {
+            this.stage = stage;
+            this.intermediate_stage = intermediate_stage;
+            this.isScheduled = isScheduled;
+            this.className = className;
+        }
+
+        public int getStage() {
+            return stage;
+        }
+
+        public int getIntermediate_stage() {
+            return intermediate_stage;
+        }
+
+        public boolean isScheduled() {
+            return isScheduled;
+        }
+
+        public String getClassName() {
+            return className;
+        }
+
+        public static EMExpStages get(int ordinal) { return values[ordinal]; }
+
+        public static int getSize() {
+            return values().length;
+        }
+
+    }
+
+    public enum EMExpStatus {
+        CREATED,
+        WAIT,
+        IN_PROGRESS,
+        COMPLETED
+    }
+
+    public static String createUUID() {
+        return UUID.randomUUID().toString();
+    }
+
+    public static String formatNSDKey(String namespace, String deploymentName) {
+        return (new StringBuilder())
+                .append(namespace)
+                .append(":")
+                .append(deploymentName)
+                .toString();
+    }
+}
