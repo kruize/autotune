a759d31d1957142fe19db6c97e62e1bba61095b7
Dinakar Guniguntala <dgunigun@redhat.com>
2021-11-15 19:23:50 +0530

    Create separate container for Java and Python Update build and deploy scripts to support the same. We will still have a single pod that will house both containers to enable the service connections to work the same.

--- scripts/minikube-helpers.sh
+++ scripts/minikube-helpers.sh
@@ -63,6 +63,7 @@ function minikube_deploy() {
 
 	# Replace autotune docker image in deployment yaml
 	sed -e "s|{{ AUTOTUNE_IMAGE }}|${AUTOTUNE_DOCKER_IMAGE}|" ${AUTOTUNE_DEPLOY_MANIFEST_TEMPLATE} > ${AUTOTUNE_DEPLOY_MANIFEST}
+	sed -i "s|{{ OPTUNA_IMAGE }}|${OPTUNA_DOCKER_IMAGE}|" ${AUTOTUNE_DEPLOY_MANIFEST}
 
 	${kubectl_cmd} apply -f ${AUTOTUNE_DEPLOY_MANIFEST}
 	sleep 2
