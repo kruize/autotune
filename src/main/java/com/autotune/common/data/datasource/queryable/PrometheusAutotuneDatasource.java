9be67a0f378dbc1e47f5e9cbced70230e03d7b62
bharathappali <bharath.appali@gmail.com>
2022-08-04 15:35:04 +0530

    Saving changes
    Signed-off-by: bharathappali <bharath.appali@gmail.com>

--- src/main/java/com/autotune/common/data/datasource/queryable/PrometheusAutotuneDatasource.java
+++ src/main/java/com/autotune/common/data/datasource/queryable/PrometheusAutotuneDatasource.java
@@ -1,16 +1,26 @@
 package com.autotune.common.data.datasource.queryable;
 
+import com.autotune.common.data.datasource.AutotuneDatasourceServiceability;
 import com.autotune.common.utils.CommonConstants;
+import com.autotune.common.utils.CommonUtils;
 
-public class PrometheusAutotuneDatasource extends QueryableAutotuneDatasource {
+public class PrometheusAutotuneDatasource extends QueryableAutotuneDatasource implements AutotuneDatasourceServiceability {
     private String name = CommonConstants.AutotuneDatasource.Prometheus.DEFAULT_NAME;
     private String source = null;
 
+    /**
+     * Get the name of the datasource
+     * @return the name of the datasource
+     */
     @Override
     public String getName() {
         return this.name;
     }
 
+    /**
+     * Sets the name of the datasource
+     * @param name Name of the datasource
+     */
     @Override
     public void setName(String name) {
         if((null != name) && (0 != name.trim().length()))
@@ -19,16 +29,42 @@ public class PrometheusAutotuneDatasource extends QueryableAutotuneDatasource {
 
     /**
      * Needs a check for null when used
-     * @return
+     * @return the source variable
      */
     @Override
     public String getSource() {
         return this.source;
     }
 
+    /**
+     * Sets the source of the datasource
+     * @param source
+     */
     @Override
     public void setSource(String source) {
         if((null != source) && (0 != source.trim().length()))
             this.source = source;
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public CommonUtils.DatasourceReachabilityStatus isReachable() {
+        if((null != source) && (0 != source.trim().length()))
+            return CommonUtils.DatasourceReachabilityStatus.SOURCE_NOT_SET;
+        /**
+         * Implementation needed for reachability
+         */
+        return CommonUtils.DatasourceReachabilityStatus.NOT_REACHABLE;
+    }
+
+    @Override
+    public CommonUtils.DatasourceReliabilityStatus isReliable(Object... objects) {
+        /**
+         * Needs appropriate implementation
+         */
+        return CommonUtils.DatasourceReliabilityStatus.NOT_RELIABLE;
     }
 }
