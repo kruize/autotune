a8e7b7cff09d41d04b601bc1cc7d95575e27c468
msvinaykumar <vinakuma@redhat.com>
2022-08-30 15:34:32 +0530

    Added constants. TrailInfo is optional now.
    Signed-off-by: msvinaykumar <vinakuma@redhat.com>

--- src/main/java/com/autotune/experimentManager/services/CreateExperimentTrial.java
+++ src/main/java/com/autotune/experimentManager/services/CreateExperimentTrial.java
@@ -60,7 +60,7 @@ public class CreateExperimentTrial extends HttpServlet {
             List<ExperimentTrial> experimentTrialList = Arrays.asList(experimentTrialArray);
             experimentTrialList.forEach(
                     (experimentTrial) -> {
-                        LOGGER.debug("Experiment name {} with trial number {}  started processing", experimentTrial.getExperimentName(), experimentTrial.getTrialInfo().getTrialNum());
+                        LOGGER.debug("Experiment name {} started processing", experimentTrial.getExperimentName());
                         new Thread() {
                             @Override
                             public void run() {
