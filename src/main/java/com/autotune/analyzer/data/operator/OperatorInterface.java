da404be47c22702aacdc6ede9d93976339298791
msvinaykumar <vinakuma@redhat.com>
2022-10-10 19:48:35 +0530

    API's to create ,update, list and recommend with dummy data.
    Signed-off-by: msvinaykumar <vinakuma@redhat.com>

---
+++ src/main/java/com/autotune/analyzer/data/operator/OperatorInterface.java
@@ -0,0 +1,11 @@
+package com.autotune.analyzer.operator;
+
+import com.autotune.analyzer.model.AutotuneOperatorData;
+
+import java.util.List;
+
+public interface OperatorInterface {
+
+    public boolean addOperator(List<AutotuneOperatorData> operatorDataList);
+
+}
--- src/main/java/com/autotune/analyzer/services/ListExperiments.java
+++ src/main/java/com/autotune/analyzer/services/ListExperiments.java
@@ -1,48 +1,90 @@
+/*******************************************************************************
+ * Copyright (c) 2022 Red Hat, IBM Corporation and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
 package com.autotune.analyzer.services;
 
 import com.autotune.analyzer.AutotuneExperiment;
 import com.autotune.analyzer.RunExperiment;
 import com.autotune.analyzer.exceptions.InvalidValueException;
+import com.autotune.common.annotations.json.AutotuneJSONExclusionStrategy;
 import com.autotune.common.experiments.ExperimentTrial;
+import com.autotune.common.target.kubernetes.service.KubernetesServices;
 import com.autotune.experimentManager.exceptions.IncompatibleInputJSONException;
 import com.autotune.utils.AnalyzerConstants;
 import com.autotune.utils.TrialHelpers;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 import static com.autotune.analyzer.Experimentator.experimentsMap;
 import static com.autotune.utils.AnalyzerConstants.ServiceConstants.*;
 import static com.autotune.utils.TrialHelpers.updateExperimentTrial;
 
+/**
+ * Rest API used to list experiments.
+ */
 public class ListExperiments extends HttpServlet {
     private static final Logger LOGGER = LoggerFactory.getLogger(ListExperiments.class);
+    ConcurrentHashMap<String, JSONObject> mainAutoTuneOperatorMap = new ConcurrentHashMap<>();
+    KubernetesServices kubernetesServices = null;
 
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        this.mainAutoTuneOperatorMap = (ConcurrentHashMap<String, JSONObject>) getServletContext().getAttribute(com.autotune.analyzer.utils.AnalyzerConstants.AnalyserKeys.ANALYSER_STORAGE_CONTEXT_KEY);
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setStatus(HttpServletResponse.SC_OK);
         response.setContentType(JSON_CONTENT_TYPE);
         response.setCharacterEncoding(CHARACTER_ENCODING);
-
-        JSONArray experimentTrialJSONArray = new JSONArray();
-        for (String deploymentName : experimentsMap.keySet()) {
-            AutotuneExperiment autotuneExperiment = experimentsMap.get(deploymentName);
-            for (int trialNum : autotuneExperiment.getExperimentTrials().keySet()) {
-                ExperimentTrial experimentTrial = autotuneExperiment.getExperimentTrials().get(trialNum);
-                JSONArray experimentTrialJSON = new JSONArray(TrialHelpers.experimentTrialToJSON(experimentTrial));
-                experimentTrialJSONArray.put(experimentTrialJSON.get(0));
+        String gsonStr = "[]";
+        if (this.mainAutoTuneOperatorMap.size() > 0) {
+            Gson gsonObj = new GsonBuilder()
+                    .disableHtmlEscaping()
+                    .setPrettyPrinting()
+                    .setExclusionStrategies(new AutotuneJSONExclusionStrategy())
+                    .create();
+            gsonStr = gsonObj.toJson(this.mainAutoTuneOperatorMap);
+        } else {
+            JSONArray experimentTrialJSONArray = new JSONArray();
+            for (String deploymentName : experimentsMap.keySet()) {
+                AutotuneExperiment autotuneExperiment = experimentsMap.get(deploymentName);
+                for (int trialNum : autotuneExperiment.getExperimentTrials().keySet()) {
+                    ExperimentTrial experimentTrial = autotuneExperiment.getExperimentTrials().get(trialNum);
+                    JSONArray experimentTrialJSON = new JSONArray(TrialHelpers.experimentTrialToJSON(experimentTrial));
+                    experimentTrialJSONArray.put(experimentTrialJSON.get(0));
+                }
             }
+            gsonStr = experimentTrialJSONArray.toString(4);
         }
-        response.getWriter().println(experimentTrialJSONArray.toString(4));
+        response.getWriter().println(gsonStr);
         response.getWriter().close();
     }
 
