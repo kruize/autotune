69403b929711553ed2b0334a53b9a8d7554ca4f1
bharathappali <abharath@redhat.com>
2022-11-02 16:05:40 +0530

    Check for parameter and then for json body
    Signed-off-by: bharathappali <abharath@redhat.com>

--- src/main/java/com/autotune/analyzer/services/ListDeploymentsInNamespace.java
+++ src/main/java/com/autotune/analyzer/services/ListDeploymentsInNamespace.java
@@ -31,38 +31,45 @@ public class ListDeploymentsInNamespace extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) {
         KubernetesServices kubernetesServices = null;
         try {
-            String inputData = request.getReader().lines().collect(Collectors.joining());
-            JSONObject inputJson = new JSONObject(inputData);
-            if (!inputJson.has(AutotuneConstants.JSONKeys.NAMESPACE)) {
-                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            } else {
-                if (null == inputJson.getString(AutotuneConstants.JSONKeys.NAMESPACE)
-                        || inputJson.getString(AutotuneConstants.JSONKeys.NAMESPACE).isBlank()
-                        || inputJson.getString(AutotuneConstants.JSONKeys.NAMESPACE).isEmpty()) {
+            String namespace = request.getParameter(AutotuneConstants.JSONKeys.NAMESPACE);
+            if (null == namespace) {
+                String inputData = request.getReader().lines().collect(Collectors.joining());
+                JSONObject inputJson = new JSONObject(inputData);
+                if (!inputJson.has(AutotuneConstants.JSONKeys.NAMESPACE)) {
                     response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                    namespace = null;
                 } else {
-                    String namespace = inputJson.getString(AutotuneConstants.JSONKeys.NAMESPACE);
-                    // Initialising the kubernetes service
-                    kubernetesServices = new KubernetesServicesImpl();
-                    // Set response headers
-                    response.addHeader("Access-Control-Allow-Origin", "*");
-                    response.addHeader("Access-Control-Allow-Methods", "POST, GET");
-                    response.addHeader("Access-Control-Allow-Headers", "X-PINGOTHER, Origin, X-Requested-With, Content-Type, Accept");
-                    response.addHeader("Access-Control-Max-Age", "1728000");
-                    JSONObject returnJson = new JSONObject();
-                    JSONObject dataJson = new JSONObject();
-                    JSONArray deploymentsList = new JSONArray();
-                    kubernetesServices.getDeploymentsBy(namespace).forEach(deployment -> {
-                        deploymentsList.put(deployment.getMetadata().getName());
-                    });
-                    dataJson.put(AutotuneConstants.JSONKeys.DEPLOYMENTS, deploymentsList);
-                    returnJson.put(AutotuneConstants.JSONKeys.DATA, dataJson);
-                    // Set content type
-                    response.setContentType("application/json");
-                    response.setCharacterEncoding("UTF-8");
-                    response.setStatus(HttpServletResponse.SC_OK);
-                    response.getWriter().println(returnJson.toString(4));
+                    if (null == inputJson.getString(AutotuneConstants.JSONKeys.NAMESPACE)
+                            || inputJson.getString(AutotuneConstants.JSONKeys.NAMESPACE).isBlank()
+                            || inputJson.getString(AutotuneConstants.JSONKeys.NAMESPACE).isEmpty()) {
+                        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                        namespace = null;
+                    } else {
+                        namespace = inputJson.getString(AutotuneConstants.JSONKeys.NAMESPACE);
+                    }
                 }
+            }
+            if (null != namespace) {
+                // Initialising the kubernetes service
+                kubernetesServices = new KubernetesServicesImpl();
+                // Set response headers
+                response.addHeader("Access-Control-Allow-Origin", "*");
+                response.addHeader("Access-Control-Allow-Methods", "POST, GET");
+                response.addHeader("Access-Control-Allow-Headers", "X-PINGOTHER, Origin, X-Requested-With, Content-Type, Accept");
+                response.addHeader("Access-Control-Max-Age", "1728000");
+                JSONObject returnJson = new JSONObject();
+                JSONObject dataJson = new JSONObject();
+                JSONArray deploymentsList = new JSONArray();
+                kubernetesServices.getDeploymentsBy(namespace).forEach(deployment -> {
+                    deploymentsList.put(deployment.getMetadata().getName());
+                });
+                dataJson.put(AutotuneConstants.JSONKeys.DEPLOYMENTS, deploymentsList);
+                returnJson.put(AutotuneConstants.JSONKeys.DATA, dataJson);
+                // Set content type
+                response.setContentType("application/json");
+                response.setCharacterEncoding("UTF-8");
+                response.setStatus(HttpServletResponse.SC_OK);
+                response.getWriter().println(returnJson.toString(4));
             }
         } catch (Exception e) {
             e.printStackTrace();
