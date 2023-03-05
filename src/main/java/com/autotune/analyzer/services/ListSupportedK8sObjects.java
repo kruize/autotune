c1f2c97460a326fd08b6ea84c59912f72e773224
bharathappali <abharath@redhat.com>
2023-03-05 20:52:50 +0530

    Add LICENSES
    Signed-off-by: bharathappali <abharath@redhat.com>

--- src/main/java/com/autotune/analyzer/services/ListSupportedK8sObjects.java
+++ src/main/java/com/autotune/analyzer/services/ListSupportedK8sObjects.java
@@ -1,6 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2023 Red Hat, IBM Corporation and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 package com.autotune.analyzer.services;
 
-import com.autotune.analyzer.exceptions.AutotuneResponse;
 import com.autotune.analyzer.serviceObjects.ListSupportedK8sObjectsSO;
 import com.autotune.analyzer.utils.GsonUTCDateAdapter;
 import com.autotune.utils.AnalyzerConstants;
@@ -16,11 +30,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.Date;
-
-import static com.autotune.utils.AnalyzerConstants.ServiceConstants.CHARACTER_ENCODING;
-import static com.autotune.utils.AnalyzerConstants.ServiceConstants.JSON_CONTENT_TYPE;
 
 @WebServlet(asyncSupported = true)
 public class ListSupportedK8sObjects extends HttpServlet {
