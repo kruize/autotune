96b662b87e1611d455531c05fe4c40a204150f4b
Chandrakala Subramanyam <csubrama@redhat.com>
2023-03-20 17:20:15 +0530

    Updated a test for PR check
    Signed-off-by: Chandrakala Subramanyam <csubrama@redhat.com>

---
+++ tests/scripts/remote_monitoring_tests/rest_apis/test_e2e_workflow.py
@@ -0,0 +1,94 @@
+import requests
+import pytest
+from jinja2 import Environment, FileSystemLoader
+from helpers.utils import *
+from helpers.list_reco_json_validate import *
+from helpers.kruize import *
+from helpers.fixtures import *
+import time
+import json
+
+@pytest.mark.test_e2e
+def test_list_recommendations_multiple_exps_from_diff_json_files(cluster_type):
+    """
+    Test Description: This test validates list recommendations for multiple experiments posted using different json files
+    """
+
+    input_json_file="../json_files/create_exp.json"
+    result_json_file="../json_files/update_results.json"
+
+    find = []
+    json_data = json.load(open(input_json_file))
+
+    find.append(json_data[0]['experiment_name'])
+    find.append(json_data[0]['kubernetes_objects'][0]['name'])
+    find.append(json_data[0]['kubernetes_objects'][0]['namespace'])
+
+    form_kruize_url(cluster_type)
+
+    # Create experiment using the specified json
+    num_exps = 100
+    for i in range(num_exps):
+        json_file = "/tmp/create_exp_" + str(i) + ".json"
+        generate_json(find, input_json_file, json_file, i)
+
+        response = delete_experiment(json_file)
+        print("delete exp = ", response.status_code)
+
+        response = create_experiment(json_file)
+
+        data = response.json()
+        print("message = ", data['message'])
+        assert response.status_code == SUCCESS_STATUS_CODE
+        assert data['status'] == SUCCESS_STATUS
+        assert data['message'] == CREATE_EXP_SUCCESS_MSG
+
+    for i in range(num_exps):
+        # Update results for the experiment
+        json_file = "/tmp/update_results_" + str(i) + ".json"
+        update_timestamps = True
+        generate_json(find, result_json_file, json_file, i, update_timestamps)
+        response = update_results(json_file)
+
+        data = response.json()
+        print("message = ", data['message'])
+        assert response.status_code == SUCCESS_STATUS_CODE
+        assert data['status'] == SUCCESS_STATUS
+        assert data['message'] == UPDATE_RESULTS_SUCCESS_MSG
+
+    time.sleep(30)
+
+    # Get the experiment name
+    json_data = json.load(open(input_json_file))
+
+    experiment_name = ""
+    response = list_recommendations(experiment_name)
+
+    list_reco_json = response.json()
+    assert response.status_code == SUCCESS_200_STATUS_CODE
+
+    # Validate the json against the json schema
+    errorMsg = validate_list_reco_json(list_reco_json)
+    assert errorMsg == ""
+
+    # Validate the json values
+    for i in range(num_exps):
+        input_json_file = "/tmp/create_exp_" + str(i) + ".json"
+        create_exp_json = read_json_data_from_file(input_json_file)
+
+        result_json_file = "/tmp/update_results_" + str(i) + ".json"
+        update_results_json = read_json_data_from_file(result_json_file) 
+
+        experiment_name = create_exp_json[0]['experiment_name']
+        response = list_recommendations(experiment_name)
+        list_reco_json = response.json()
+
+        validate_reco_json(create_exp_json[0], update_results_json[0], list_reco_json[0])
+
+    # Delete the experiments    
+    for i in range(num_exps):
+        json_file = "/tmp/create_exp_" + str(i) + ".json"
+
+        response = delete_experiment(json_file)
+        print("delete exp = ", response.status_code)
+
