3f5b25882ea5e41055d4bbc2b03b3801d8ad6722
Chandrakala Subramanyam <csubrama@redhat.com>
2023-04-19 16:59:39 +0530

    Included a test to validate kruize pod restart behaviour
    Signed-off-by: Chandrakala Subramanyam <csubrama@redhat.com>

---
+++ tests/scripts/remote_monitoring_tests/fault_tolerant_tests/kruize_pod_restart_test.py
@@ -0,0 +1,181 @@
+"""
+Copyright (c) 2023, 2023 Red Hat, IBM Corporation and others.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import sys, getopt
+import json
+import os
+import time
+sys.path.append("..")
+from helpers.kruize import *
+from helpers.utils import *
+from helpers.generate_rm_jsons import *
+
+def main(argv):
+    cluster_type = "minikube"
+    results_dir = "."
+    hours = 6
+    num_exps = 3
+    failed = 0
+    try:
+        opts, args = getopt.getopt(argv,"h:c:a:u:r:d:")
+    except getopt.GetoptError:
+        print("kruize_pod_restart_test.py -c <cluster type> -a <openshift kruize route> -u <no. of experiments> -d <hours of data available> -r <results dir>")
+        print("Note: -a option is required only on openshift when kruize service is exposed")
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print("kruize_pod_restart_test.py -c <cluster type> -a <openshift kruize route> -u <no. of experiments> -d <hours of data available> -r <results dir>")
+            sys.exit(0)
+        elif opt == '-c':
+            cluster_type = arg
+        elif opt == '-a':
+            server_ip_addr = arg
+        elif opt == '-u':
+            num_exps = int(arg)
+        elif opt == '-r':
+            results_dir = arg
+        elif opt == '-d':
+            hours = int(arg)
+        
+
+    print(f"Cluster type = {cluster_type}")
+    print(f"No. of experiments = {num_exps}")
+    print(f"Results dir = {results_dir}")
+
+    # Form the kruize url
+    if cluster_type == "minikube":
+        form_kruize_url(cluster_type)
+    else:
+        form_kruize_url(cluster_type, server_ip_addr)
+
+    # Create the performance profile
+    perf_profile_json_file = "../json_files/resource_optimization_openshift.json"
+    create_performance_profile(perf_profile_json_file)
+
+    # Generate pay load 
+    csv_filename = "../csv_data/tfb_data.csv"
+    split = False
+    split_count = 1
+
+    exp_json_dir = results_dir + "/exp_jsons" + "_" + str(num_exps)
+    result_json_dir = results_dir + "/result_jsons" + "_" + str(num_exps)
+    reco_json_dir = results_dir + "/reco_jsons_" + str(num_exps)
+    os.mkdir(reco_json_dir)
+
+    exp_list = []
+
+    iterations = int(hours / 6)
+    # 6 hour results of each result with 15mins duration, so no. of results 6 * 4
+    num_res = 24 
+
+    for i in range(1, iterations+1):
+        print("\n*************************")
+        print(f"Iteration {i}...")
+        print("*************************\n")
+        create_exp_jsons(split, split_count, exp_json_dir, num_exps)
+
+        if i == 1:
+            new_timestamp = None
+            create_update_results_jsons(csv_filename, split, split_count, result_json_dir, num_exps, num_res, new_timestamp)
+            start_ts = get_datetime()
+        else:
+            # Increment the time by 365 mins or 6 hrs 6 mins for the next set of data timestamps
+            new_timestamp = increment_timestamp_by_given_mins(start_ts, 365)
+            start_ts = new_timestamp
+            create_update_results_jsons(csv_filename, split, split_count, result_json_dir, num_exps, num_res, new_timestamp)
+
+        for res_num in range(num_res):
+            for exp_num in range(num_exps):
+                # create the experiment and post it
+                create_exp_json_file = exp_json_dir + "/create_exp_" + str(exp_num) + ".json"
+                create_experiment(create_exp_json_file)
+                
+                # Obtain the experiment name
+                json_data = json.load(open(create_exp_json_file))
+
+                experiment_name = json_data[0]['experiment_name']
+                print(f"experiment_name = {experiment_name}")
+
+                # update 6 hours result for the specified experiment
+                json_file = result_json_dir + "/result_" + str(exp_num) + "_" + str(res_num) + ".json"
+
+                update_results(json_file)
+
+                # Obtain the monitoring end time
+                json_data = json.load(open(json_file))
+                interval_end_time = json_data[0]['interval_end_time']
+
+                # sleep for a while before fetching recommendations for the experiments
+                #time.sleep(1)
+
+                # Fetch the recommendations for all the experiments
+                latest = None
+                reco = list_recommendations(experiment_name, latest, interval_end_time)
+                filename = reco_json_dir + '/reco_' + experiment_name + '.json'
+                write_json_data_to_file(filename, reco.json())
+
+        time.sleep(10)
+        # Fetch listExperiments
+        list_exp_json_file_before = results_dir + "/list_exp_json_before_" + str(i) + ".json"
+        response = list_experiments()
+        list_exp_json = response.json()
+
+        write_json_data_to_file(list_exp_json_file_before, list_exp_json)
+
+        if cluster_type == "minikube":
+            namespace = "monitoring"
+        elif cluster_type == "openshift":
+            namespace = "openshift-tuning"
+
+        # Delete the kruize pod
+        delete_kruize_pod(namespace)
+
+        # Check if the kruize pod is running
+        pod_name = get_kruize_pod(namespace)
+        result = check_pod_running(namespace, pod_name)
+
+        if result == False:
+            print("Restarting kruize failed!")
+            failed = 1
+            sys.exit(failed)
+
+        # Fetch listExperiments
+        list_exp_json_file_after = results_dir + "/list_exp_json_after_" + str(i) + ".json"
+        response = list_experiments()
+        list_exp_json = response.json()
+
+        write_json_data_to_file(list_exp_json_file_after, list_exp_json)
+
+        # Compare the listExperiments before and after kruize pod restart
+        result = compare_json_files(list_exp_json_file_before, list_exp_json_file_after)
+        if result == True:
+            print("Test Passed! listExperiments before and after kruize pod restart are same!")
+        else:
+            failed = 1
+            print("Test Failed! listExperiments before and after kruize pod restart are not same!")
+
+        # sleep for a while to mimic the availability of next set of results
+        time.sleep(60)
+
+    for exp_num in range(num_exps):
+        # Delete the experiment
+        create_exp_json_file = exp_json_dir + "/create_exp_" + str(exp_num) + ".json"
+        delete_experiment(create_exp_json_file)
+
+    sys.exit(failed)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
---
+++ tests/scripts/remote_monitoring_tests/fault_tolerant_tests/remote_monitoring_fault_tolerant_tests.sh
@@ -0,0 +1,163 @@
+#!/bin/bash
+#
+# Copyright (c) 2023, 2023 IBM Corporation, RedHat and others.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+### Script to run fault tolerant tests with Kruize in remote monitoring mode ##
+#
+
+CURRENT_DIR="$(dirname "$(realpath "$0")")"
+KRUIZE_REPO="${CURRENT_DIR}/../../../../"
+
+
+# Source the common functions scripts
+. ${CURRENT_DIR}/../../common/common_functions.sh
+
+
+RESULTS_DIR=/tmp/kruize_fault_tolerant_test_results
+APP_NAME=kruize
+CLUSTER_TYPE=minikube
+NAMESPACE=monitoring
+num_exps=3
+
+RESOURCE_OPTIMIZATION_JSON="../json_files/resource_optimization_openshift.json"
+
+target="crc"
+KRUIZE_IMAGE="kruize/autotune_operator:test"
+hours=6
+
+function usage() {
+	echo
+	echo "Usage: -c cluster_tyep[minikube|openshift] [-i Kruize image] [-u No. of experiments] [-d hours of data available] [-r <resultsdir path>]"
+	exit -1
+}
+
+function get_kruize_pod_log() {
+	log=$1
+
+	# Fetch the kruize pod log
+
+	echo ""
+	echo "Fetch the kruize pod logs and store in ${log}..."
+	kruize_pod=$(kubectl get pod -n ${NAMESPACE} | grep kruize | cut -d " " -f1)
+	kubectl logs -f ${kruize_pod} -n ${NAMESPACE} > ${log} 2>&1 &
+}
+
+while getopts c:r:i:u:d:t:h gopts
+do
+	case ${gopts} in
+	c)
+		CLUSTER_TYPE=${OPTARG}
+		;;
+	r)
+		RESULTS_DIR="${OPTARG}"		
+		;;
+	i)
+		KRUIZE_IMAGE="${OPTARG}"		
+		;;
+	u)
+		num_exps="${OPTARG}"		
+		;;
+	d)
+		hours="${OPTARG}"		
+		;;
+	h)
+		usage
+		;;
+	esac
+done
+
+if [ -z "${CLUSTER_TYPE}" ]; then
+	usage
+fi
+
+start_time=$(get_date)
+LOG_DIR="${RESULTS_DIR}/remote-monitoring-fault-tolerant-test-$(date +%Y%m%d%H%M)"
+mkdir -p ${LOG_DIR}
+
+LOG="${LOG_DIR}/remote-monitoring-fault-tolerant-test.log"
+
+prometheus_pod_running=$(kubectl get pods --all-namespaces | grep "prometheus-k8s-0")
+if [ "${prometheus_pod_running}" == "" ]; then
+	echo "Install prometheus required to fetch the resource usage metrics for kruize"
+	exit 1
+
+fi
+
+KRUIZE_SETUP_LOG="${LOG_DIR}/kruize_setup.log"
+KRUIZE_POD_LOG="${LOG_DIR}/kruize_pod.log"
+
+# Setup kruize
+echo "Setting up kruize..." | tee -a ${LOG}
+pushd ${KRUIZE_REPO} > /dev/null
+        echo "./deploy.sh -c ${CLUSTER_TYPE} -i ${KRUIZE_IMAGE} -m ${target} -t >> ${KRUIZE_SETUP_LOG}" | tee -a ${LOG}
+        ./deploy.sh -c ${CLUSTER_TYPE} -i ${KRUIZE_IMAGE} -m ${target} -t >> ${KRUIZE_SETUP_LOG} 2>&1
+
+        sleep 20
+        echo "./deploy.sh -c ${CLUSTER_TYPE} -i ${KRUIZE_IMAGE} -m ${target} >> ${KRUIZE_SETUP_LOG}" | tee -a ${LOG}
+        ./deploy.sh -c ${CLUSTER_TYPE} -i ${KRUIZE_IMAGE} -m ${target} >> ${KRUIZE_SETUP_LOG} 2>&1
+        sleep 20
+popd > /dev/null
+echo "Setting up kruize...Done" | tee -a ${LOG}
+
+case ${CLUSTER_TYPE} in
+	minikube)
+		if [ -z "${SERVER_IP_ADDR}" ]; then
+			SERVER_IP_ADDR=$(minikube ip)
+			echo "Port forward prometheus..." | tee -a ${LOG}
+			kubectl port-forward svc/prometheus-k8s 9090:9090 -n ${NAMESPACE} > /dev/null 2>/dev/null &
+			echo "Port forward prometheus...done" | tee -a ${LOG}
+			port=$(kubectl -n ${NAMESPACE} get svc ${APP_NAME} --no-headers -o=custom-columns=PORT:.spec.ports[*].nodePort)
+			if [ "${port}" == "" ]; then
+				echo "Failed to get the Kruize port, Check if kruize is runnning!" | tee -a ${LOG}
+				exit -1
+			fi
+			echo "SERVER_IP_ADDR = ${SERVER_IP_ADDR} port = ${port}" | tee -a ${LOG}
+		fi
+		;;
+	openshift)
+		NAMESPACE="openshift-tuning"
+		if [ -z "${SERVER_IP_ADDR}" ]; then
+			oc expose svc/kruize -n ${NAMESPACE}
+
+			SERVER_IP_ADDR=($(oc status --namespace=${NAMESPACE} | grep "kruize" | grep port | cut -d " " -f1 | cut -d "/" -f3))
+			port=""
+			echo "SERVER_IP_ADDR = ${SERVER_IP_ADDR}" | tee -a ${LOG}
+		fi
+		;;
+	*)
+		err_exit "Error: Cluster type ${CLUSTER_TYPE} is not supported" | tee -a ${LOG}
+		;;
+esac	
+
+echo | tee -a ${LOG}
+
+get_kruize_pod_log ${KRUIZE_POD_LOG}
+
+# Run the test
+TEST_LOG="${LOG_DIR}/kruize_pod_restart_test.log"
+echo ""
+echo "Running fault tolerant test for kruize on ${CLUSTER_TYPE}" | tee -a ${LOG}
+if [ "${CLUSTER_TYPE}" == "openshift" ]; then
+	echo "python3 kruize_pod_restart_test.py -c ${CLUSTER_TYPE} -a ${SERVER_IP_ADDR} -u ${num_exps} -d ${hours} -r ${LOG_DIR} | tee -a  ${TEST_LOG}" | tee -a ${LOG}
+	python3 kruize_pod_restart_test.py -c ${CLUSTER_TYPE} -a ${SERVER_IP_ADDR} -u ${num_exps} -d ${hours} -r "${LOG_DIR}" | tee -a ${TEST_LOG}
+
+else
+	echo "python3 kruize_pod_restart_test.py -c ${CLUSTER_TYPE} -u ${num_exps} -d ${hours} -r ${LOG_DIR} | tee -a  ${TEST_LOG}" | tee -a ${LOG}
+	python3 kruize_pod_restart_test.py -c ${CLUSTER_TYPE} -u ${num_exps} -d ${hours} -r "${LOG_DIR}" | tee -a ${TEST_LOG}
+fi
+
+end_time=$(get_date)
+elapsed_time=$(time_diff "${start_time}" "${end_time}")
+echo "Test took ${elapsed_time} seconds to complete" | tee -a ${LOG}
