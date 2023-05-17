669c98b57c3b302dc9fc30636d22fdb7cd35f497
kusumachalasani <kchalasa@redhat.com>
2023-05-17 15:03:25 +0530

    Use micrometer prometheus registry metrics
    Signed-off-by: kusumachalasani <kchalasa@redhat.com>

--- src/main/java/com/autotune/utils/MetricsConfig.java
+++ src/main/java/com/autotune/utils/MetricsConfig.java
@@ -2,8 +2,10 @@ package com.autotune.utils;
 
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Timer;
+import io.micrometer.core.instrument.binder.jvm.ClassLoaderMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics;
 import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics;
+import io.micrometer.core.instrument.binder.system.ProcessorMetrics;
 import io.micrometer.core.instrument.config.MeterFilter;
 import io.micrometer.core.instrument.config.NamingConvention;
 import io.micrometer.prometheus.PrometheusConfig;
@@ -12,21 +14,28 @@ import io.prometheus.client.exporter.MetricsServlet;
 import io.prometheus.client.hotspot.*;
 
 public class MetricsConfig {
-    public MeterRegistry meterRegistry() {
-        PrometheusMeterRegistry registry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
+
+    public PrometheusMeterRegistry registry;
+
+    public MetricsConfig() {
+        createMeterRegistry();
+    }
+
+    public void createMeterRegistry() {
+        registry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
         Timer timer = Timer.builder("my.timer").description("A timer metric").register(registry);
         registry.config().commonTags("application", "Kruize");
-        registry.config().meterFilter(MeterFilter.deny(id -> {
-                      String uri = id.getTag("uri");
-                      return uri != null && uri.startsWith("/health");
-                 }));
+        //registry.config().meterFilter(MeterFilter.deny(id -> {
+        //    String uri = id.getTag("uri");
+        //    return uri != null && uri.startsWith("/health");
+        //}));
+        new ClassLoaderMetrics().bindTo(registry);
+        new ProcessorMetrics().bindTo(registry);
         new JvmGcMetrics().bindTo(registry);
         new JvmMemoryMetrics().bindTo(registry);
+        registry.config().namingConvention(NamingConvention.dot);
         //registry.config().meterFilter(MeterFilter.ignoreTags("http.method"))
-        registry.config().namingConvention(NamingConvention.dot)
- //       registry.config().meterFilter(MeterFilter.maximumAllowableTags("http.path", 2, MeterFilter.DenyTagValueMeterFilter.onDiscardOldest()));
-
-        return registry;
+        //registry.config().meterFilter(MeterFilter.maximumAllowableTags("http.path", 2, MeterFilter.DenyTagValueMeterFilter.onDiscardOldest()));
     }
 
 }
