3223bfbd06dc97cd6daa7aeb545b377209c827cf
msvinaykumar <vinakuma@redhat.com>
2023-05-27 15:22:49 +0530

    To restrict the number of rows in the result set, the Load results operation involves locating the appropriate method and configuring the desired limitation. It's important to note that in order for the Limit rows feature to function correctly, the CreateExperiment API must adhere strictly to the trail settings' measurement duration and should not allow arbitrary values
    Signed-off-by: msvinaykumar <vinakuma@redhat.com>

--- src/main/java/com/autotune/database/dao/ExperimentDAO.java
+++ src/main/java/com/autotune/database/dao/ExperimentDAO.java
@@ -47,7 +47,7 @@ public interface ExperimentDAO {
     List<KruizeExperimentEntry> loadExperimentByName(String experimentName) throws Exception;
 
     // Load all results for a particular experimentName
-    List<KruizeResultsEntry> loadResultsByExperimentName(String experimentName, Timestamp interval_start_time, Timestamp interval_end_time) throws Exception;
+    List<KruizeResultsEntry> loadResultsByExperimentName(String experimentName, Timestamp interval_start_time, Integer limitRows) throws Exception;
 
     // Load all recommendations of a particular experiment
     List<KruizeRecommendationEntry> loadRecommendationsByExperimentName(String experimentName) throws Exception;
