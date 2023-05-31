cd369899d950be5cde51c16e4d8144b2f2f6a828
kusumachalasani <kchalasa@redhat.com>
2023-05-31 12:26:42 +0530

    Add tolerance for metrics match
    Signed-off-by: kusumachalasani <kchalasa@redhat.com>

--- tests/scripts/remote_monitoring_tests/helpers/utils.py
+++ tests/scripts/remote_monitoring_tests/helpers/utils.py
@@ -35,32 +35,32 @@ EXP_EXISTS_MSG = "Experiment name already exists: "
 INVALID_DEPLOYMENT_TYPE_MSG = "Invalid deployment type: xyz"
 INVALID_INTERVAL_DURATION_MSG = "Interval duration cannot be less than or greater than measurement_duration by more than 30 seconds"
 
-createExperimentCount_STATUS = "Metric Category: API, Metric Type: count, Name: createExperiment, Value: 10.0"
-createExperimentSum_STATUS = "Metric Category: API, Metric Type: sum, Name: createExperiment, Value: 0.228229311"
-listRecommendationsCount_STATUS = "Metric Category: API, Metric Type: count, Name: listRecommendations, Value: 11.0"
-listRecommendationsSum_STATUS = "Metric Category: API, Metric Type: sum, Name: listRecommendations, Value: 0.349422898"
-listExperimentsCount_STATUS = "Metric Category: API, Metric Type: count, Name: listExperiments, Value: 0.0"
-listExperimentsSum_STATUS = "Metric Category: API, Metric Type: sum, Name: listExperiments, Value: 0.0"
-updateResultsCount_STATUS = "Metric Category: API, Metric Type: count, Name: updateResults, Value: 1000.0"
-updateResultsSum_STATUS = "Metric Category: API, Metric Type: sum, Name: updateResults, Value: 51.24196982"
-addRecommendationToDBcount_STATUS = "Metric Category: DB, Metric Type: count, Name: addRecommendationToDB, Value: 50.0"
-addRecommendationToDBsum_STATUS = "Metric Category: DB, Metric Type: sum, Name: addRecommendationToDB, Value: 0.11283727"
-addResultsToDBCount_STATUS = "Metric Category: DB, Metric Type: count, Name: addResultsToDB, Value: 1000.0"
-addResultsToDBSum_STATUS = "Metric Category: DB, Metric Type: sum, Name: addResultsToDB, Value: 3.513978451"
-loadAllRecommendationsCount_STATUS = "Metric Category: DB, Metric Type: count, Name: loadAllRecommendations, Value: 0.0"
-loadAllRecommendationsSum_STATUS = "Metric Category: DB, Metric Type: sum, Name: loadAllRecommendations, Value: 0.0"
-loadAllExperimentsCount_STATUS = "Metric Category: DB, Metric Type: count, Name: loadAllExperiments, Value: 0.0"
-loadAllExperimentsSum_STATUS = "Metric Category: DB, Metric Type: sum, Name: loadAllExperiments, Value: 0.0"
-addExperimentToDBCount_STATUS = "Metric Category: DB, Metric Type: count, Name: addExperimentToDB, Value: 10.0"
-addExperimentToDBSum_STATUS = "Metric Category: DB, Metric Type: sum, Name: addExperimentToDB, Value: 0.132267294"
-loadResultsByExperimentNameCount_STATUS = "Metric Category: DB, Metric Type: count, Name: loadResultsByExperimentName, Value: 1000.0"
-loadResultsByExperimentNameSum_STATUS = "Metric Category: DB, Metric Type: sum, Name: loadResultsByExperimentName, Value: 16.355101603"
-loadExperimentByNameCount_STATUS = "Metric Category: DB, Metric Type: count, Name: loadExperimentByName, Value: 1031.0"
-loadExperimentByNameSum_STATUS = "Metric Category: DB, Metric Type: sum, Name: loadExperimentByName, Value: 1.665818586"
-loadAllResultsCount_STATUS = "Metric Category: DB, Metric Type: count, Name: loadAllResults, Value: 0.0"
-loadAllResultsSum_STATUS = "Metric Category: DB, Metric Type: sum, Name: loadAllResults, Value: 0.0"
-loadRecommendationsByExperimentNameCount_STATUS = "Metric Category: DB, Metric Type: count, Name: loadRecommendationsByExperimentName, Value: 11.0"
-loadRecommendationsByExperimentNameSum_STATUS = "Metric Category: DB, Metric Type: sum, Name: loadRecommendationsByExperimentName, Value: 0.090203608"
+createExperimentCount_STATUS = "Metric Category: API, Name: createExperiment_count, Value: 10.0"
+createExperimentSum_STATUS = "Metric Category: API, Name: createExperiment_sum, Value: 0.228229311"
+listRecommendationsCount_STATUS = "Metric Category: API, Name: listRecommendations_count, Value: 11.0"
+listRecommendationsSum_STATUS = "Metric Category: API, Name: listRecommendations_sum, Value: 0.349422898"
+listExperimentsCount_STATUS = "Metric Category: API, Name: listExperiments_count, Value: 0.0"
+listExperimentsSum_STATUS = "Metric Category: API, Name: listExperiments_sum, Value: 0.0"
+updateResultsCount_STATUS = "Metric Category: API, Name: updateResults_count, Value: 1000.0"
+updateResultsSum_STATUS = "Metric Category: API, Name: updateResults_sum, Value: 51.24196982"
+addRecommendationToDBCount_STATUS = "Metric Category: DB, Name: addRecommendationToDB_count, Value: 50.0"
+addRecommendationToDBSum_STATUS = "Metric Category: DB, Name: addRecommendationToDB_sum, Value: 0.11283727"
+addResultsToDBCount_STATUS = "Metric Category: DB, Name: addResultsToDB_count, Value: 1000.0"
+addResultsToDBSum_STATUS = "Metric Category: DB, Name: addResultsToDB_sum, Value: 3.513978451"
+loadAllRecommendationsCount_STATUS = "Metric Category: DB, Name: loadAllRecommendations_count, Value: 0.0"
+loadAllRecommendationsSum_STATUS = "Metric Category: DB, Name: loadAllRecommendations_sum, Value: 0.0"
+loadAllExperimentsCount_STATUS = "Metric Category: DB, Name: loadAllExperiments_count, Value: 0.0"
+loadAllExperimentsSum_STATUS = "Metric Category: DB, Name: loadAllExperiments_sum, Value: 0.0"
+addExperimentToDBCount_STATUS = "Metric Category: DB, Name: addExperimentToDB_count, Value: 10.0"
+addExperimentToDBSum_STATUS = "Metric Category: DB, Name: addExperimentToDB_sum, Value: 0.132267294"
+loadResultsByExperimentNameCount_STATUS = "Metric Category: DB, Name: loadResultsByExperimentName_count, Value: 1000.0"
+loadResultsByExperimentNameSum_STATUS = "Metric Category: DB, Name: loadResultsByExperimentName_sum, Value: 16.355101603"
+loadExperimentByNameCount_STATUS = "Metric Category: DB, Name: loadExperimentByName_count, Value: 1031.0"
+loadExperimentByNameSum_STATUS = "Metric Category: DB, Name: loadExperimentByName_sum, Value: 1.665818586"
+loadAllResultsCount_STATUS = "Metric Category: DB, Name: loadAllResults_count, Value: 0.0"
+loadAllResultsSum_STATUS = "Metric Category: DB, Name: loadAllResults_sum, Value: 0.0"
+loadRecommendationsByExperimentNameCount_STATUS = "Metric Category: DB, Name: loadRecommendationsByExperimentName_count, Value: 11.0"
+loadRecommendationsByExperimentNameSum_STATUS = "Metric Category: DB, Name: loadRecommendationsByExperimentName_sum, Value: 0.090203608"
 
 time_log_csv = "/tmp/time_log.csv"
 
@@ -422,34 +422,52 @@ def match_metrics(output_metrics):
     metrics = []
     for metric in match_metrics:
         metric_category, metric_type, name, http_method, value = metric
-        metrics = f"Metric Category: {metric_category}, Metric Type: {metric_type}, Name: {name}"
-        metrics += f", Value: {value}"
-        metrics.append(metrics)
+        metrics_str = f"Metric Category: {metric_category}, Name: {name}_{metric_type}, Value: {value}"
+        metrics.append(metrics_str)
 
-    assert createExperimentCount_STATUS in metrics, "createExperimentCount_STATUS assertion failed"
-    assert createExperimentSum_STATUS in metrics, "createExperimentSum_STATUS assertion failed"
-    assert listRecommendationsCount_STATUS in metrics, "listRecommendationsCount_STATUS assertion failed"
-    assert listRecommendationsSum_STATUS in metrics, "listRecommendationsSum_STATUS assertion failed"
-    assert listExperimentsCount_STATUS in metrics, "listExperimentsCount_STATUS assertion failed"
-    assert listExperimentsSum_STATUS in metrics, "listExperimentsSum_STATUS assertion failed"
-    assert updateResultsCount_STATUS in metrics, "updateResultsCount_STATUS assertion failed"
-    assert updateResultsSum_STATUS in metrics, "updateResultsSum_STATUS assertion failed"
-    assert addRecommendationToDBcount_STATUS in metrics, "addRecommendationToDBcount_STATUS assertion failed"
-    assert addRecommendationToDBsum_STATUS in metrics, "addRecommendationToDBsum_STATUS assertion failed"
-    assert addResultsToDBCount_STATUS in metrics, "addResultsToDBCount_STATUS assertion failed"
-    assert addResultsToDBSum_STATUS in metrics, "addResultsToDBSum_STATUS assertion failed"
-    assert loadAllRecommendationsCount_STATUS in metrics, "loadAllRecommendationsCount_STATUS assertion failed"
-    assert loadAllRecommendationsSum_STATUS in metrics, "loadAllRecommendationsSum_STATUS assertion failed"
-    assert loadAllExperimentsCount_STATUS in metrics, "loadAllExperimentsCount_STATUS assertion failed"
-    assert loadAllExperimentsSum_STATUS in metrics, "loadAllExperimentsSum_STATUS assertion failed"
-    assert addExperimentToDBCount_STATUS in metrics, "addExperimentToDBCount_STATUS assertion failed"
-    assert addExperimentToDBSum_STATUS in metrics, "addExperimentToDBSum_STATUS assertion failed"
-    assert loadResultsByExperimentNameCount_STATUS in metrics, "loadResultsByExperimentNameCount_STATUS assertion failed"
-    assert loadResultsByExperimentNameSum_STATUS in metrics, "loadResultsByExperimentNameSum_STATUS assertion failed"
-    assert loadExperimentByNameCount_STATUS in metrics, "loadExperimentByNameCount_STATUS assertion failed"
-    assert loadExperimentByNameSum_STATUS in metrics, "loadExperimentByNameSum_STATUS assertion failed"
-    assert loadAllResultsCount_STATUS in metrics, "loadAllResultsCount_STATUS assertion failed"
-    assert loadAllResultsSum_STATUS in metrics, "loadAllResultsSum_STATUS assertion failed"
-    assert loadRecommendationsByExperimentNameCount_STATUS in metrics, "loadRecommendationsByExperimentNameCount_STATUS assertion failed"
-    assert loadRecommendationsByExperimentNameSum_STATUS in metrics, "loadRecommendationsByExperimentNameSum_STATUS assertion failed"
+    assertions = [
+        ("createExperiment_count", createExperimentCount_STATUS, 0),
+        ("createExperiment_sum", createExperimentSum_STATUS, 0.5),
+        ("listRecommendations_count", listRecommendationsCount_STATUS, 0),
+        ("listRecommendations_sum", listRecommendationsSum_STATUS, 0.5),
+        ("listExperiments_count", listExperimentsCount_STATUS, 0),
+        ("listExperiments_sum", listExperimentsSum_STATUS, 0),
+        ("updateResults_count", updateResultsCount_STATUS, 0),
+        ("updateResults_sum", updateResultsSum_STATUS, 30),
+        ("addRecommendationToDB_count", addRecommendationToDBCount_STATUS, 0),
+        ("addRecommendationToDB_sum", addRecommendationToDBSum_STATUS, 0.5),
+        ("addResultsToDB_count", addResultsToDBCount_STATUS, 0),
+        ("addResultsToDB_sum", addResultsToDBSum_STATUS, 5),
+        ("loadAllRecommendations_count", loadAllRecommendationsCount_STATUS, 0),
+        ("loadAllRecommendations_sum", loadAllRecommendationsSum_STATUS, 0),
+        ("loadAllExperiments_count", loadAllExperimentsCount_STATUS, 0),
+        ("loadAllExperiments_sum", loadAllExperimentsSum_STATUS, 0),
+        ("addExperimentToDB_count", addExperimentToDBCount_STATUS, 0),
+        ("addExperimentToDB_sum", addExperimentToDBSum_STATUS, 0.5),
+        ("loadResultsByExperimentName_count", loadResultsByExperimentNameCount_STATUS, 0),
+        ("loadResultsByExperimentName_sum", loadResultsByExperimentNameSum_STATUS, 15),
+        ("loadExperimentByName_count", loadExperimentByNameCount_STATUS, 0),
+        ("loadExperimentByName_sum", loadExperimentByNameSum_STATUS, 2),
+        ("loadAllResults_count", loadAllResultsCount_STATUS, 0),
+        ("loadAllResults_sum", loadAllResultsSum_STATUS, 0),
+        ("loadRecommendationsByExperimentName_count", loadRecommendationsByExperimentNameCount_STATUS, 0),
+        ("loadRecommendationsByExperimentName_sum", loadRecommendationsByExperimentNameSum_STATUS, 0.5)
+    ]
 
+    for assertion in assertions:
+        variable_name, status_string, tolerance = assertion
+
+        found_metric = None
+        for metric in metrics:
+            metric_info = metric.split(",")
+            for info in metric_info:
+                if "Name:" in info:
+                    name_value = info.split(":")[-1].strip()
+                    if name_value == variable_name:
+                        found_metric = metric
+                        break
+
+        if found_metric is not None:
+            metric_value = float(found_metric.split("Value:")[-1].strip())
+            status_value = float(status_string.split("Value:")[-1].strip())
+            assert abs(metric_value - status_value) <= tolerance, f"{variable_name} assertion failed. Expected: {status_value}, Actual: {metric_value}, Tolerance: {tolerance}"
