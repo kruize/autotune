2dfc303f2067b602118d5a128b12eecce04f5323
msvinaykumar <vinakuma@redhat.com>
2023-07-20 20:44:48 +0530

    endDate is not mandatory
    Signed-off-by: msvinaykumar <vinakuma@redhat.com>

--- src/main/java/com/autotune/analyzer/services/UpdateRecommendations.java
+++ src/main/java/com/autotune/analyzer/services/UpdateRecommendations.java
@@ -174,7 +174,6 @@ public class UpdateRecommendations extends HttpServlet {
                         experiment_name,
                         interval_start_time,
                         interval_end_time);
-
                 sendErrorResponse(response, null, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage());
                 return;
 
