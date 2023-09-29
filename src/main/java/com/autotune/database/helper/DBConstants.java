9632fe6976d25b2447bccc62eeafacc7c523e3df
Shreya <shbirada@redhat.com>
2023-09-29 14:15:14 +0530

    refactor dbconstants for cluster_name

--- src/main/java/com/autotune/database/helper/DBConstants.java
+++ src/main/java/com/autotune/database/helper/DBConstants.java
@@ -19,19 +19,21 @@ public class DBConstants {
                         KruizeConstants.JSONKeys.CALCULATED_START_TIME,
                         KruizeConstants.JSONKeys.INTERVAL_END_TIME);
         public static final String SELECT_FROM_RESULTS_BY_EXP_NAME_AND_START_END_TIME = String.format("from KruizeResultsEntry k " +
-                        "WHERE k.experiment_name = :%s and k.interval_start_time >= :%s and " +
+                        "WHERE k.cluster_name = :%s k.experiment_name = :%s and k.interval_start_time >= :%s and " +
                         "k.interval_end_time <= :%s ",
-                KruizeConstants.JSONKeys.EXPERIMENT_NAME, KruizeConstants.JSONKeys.INTERVAL_START_TIME, KruizeConstants.JSONKeys.INTERVAL_END_TIME);
+                KruizeConstants.JSONKeys.CLUSTER_NAME, KruizeConstants.JSONKeys.EXPERIMENT_NAME, KruizeConstants.JSONKeys.INTERVAL_START_TIME, KruizeConstants.JSONKeys.INTERVAL_END_TIME);
         public static final String SELECT_FROM_RESULTS_BY_EXP_NAME_AND_END_TIME = String.format(
                 "from KruizeResultsEntry k WHERE " +
+                        "k.cluster_name = :%s and " +
                         "k.experiment_name = :%s " +
                         "and k.interval_end_time = :%s ",
-                KruizeConstants.JSONKeys.EXPERIMENT_NAME, KruizeConstants.JSONKeys.INTERVAL_END_TIME);
+                KruizeConstants.JSONKeys.CLUSTER_NAME, KruizeConstants.JSONKeys.EXPERIMENT_NAME, KruizeConstants.JSONKeys.INTERVAL_END_TIME);
         public static final String SELECT_FROM_RESULTS_BY_EXP_NAME_AND_MAX_END_TIME = String.format(
                 "from KruizeResultsEntry k WHERE " +
+                        "k.cluster_name = :%s and " +
                         "k.experiment_name = :%s and " +
                         "k.interval_end_time = (SELECT MAX(e.interval_end_time) FROM KruizeResultsEntry e  where e.experiment_name = :%s ) ",
-                KruizeConstants.JSONKeys.EXPERIMENT_NAME, KruizeConstants.JSONKeys.EXPERIMENT_NAME);
+                KruizeConstants.JSONKeys.CLUSTER_NAME, KruizeConstants.JSONKeys.EXPERIMENT_NAME, KruizeConstants.JSONKeys.EXPERIMENT_NAME);
         public static final String SELECT_FROM_RECOMMENDATIONS_BY_EXP_NAME = String.format("from KruizeRecommendationEntry k WHERE k.experiment_name = :experimentName");
         public static final String SELECT_FROM_RECOMMENDATIONS_BY_EXP_NAME_AND_END_TIME = String.format("from KruizeRecommendationEntry k WHERE k.cluster_name= :%s and k.experiment_name = :%s and k.interval_end_time= :%s", KruizeConstants.JSONKeys.CLUSTER_NAME, KruizeConstants.JSONKeys.EXPERIMENT_NAME, KruizeConstants.JSONKeys.INTERVAL_END_TIME);
         public static final String SELECT_FROM_RECOMMENDATIONS_BY_CLUSTER_NAME = "from KruizeRecommendationEntry k WHERE k.cluster_name = :clusterName";
