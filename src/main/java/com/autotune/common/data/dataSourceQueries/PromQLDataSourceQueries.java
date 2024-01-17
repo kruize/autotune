b3722ba8ab87cf45ffcd1f1ab9da8f5efecd90e6
Shreya <shbirada@redhat.com>
2024-01-17 19:29:28 +0530

    Add abstraction for PromQL queries

---
+++ src/main/java/com/autotune/common/data/dataSourceQueries/PromQLDataSourceQueries.java
@@ -0,0 +1,9 @@
+package com.autotune.common.data.dataSourceQueries;
+
+public class PromQLDataSourceQueries {
+    public static final String CLUSTER_QUERY = DataSourceQueries.PromQLQuery.CLUSTER_QUERY.getQuery();
+    public static final String NAMESPACE_QUERY = DataSourceQueries.PromQLQuery.NAMESPACE_QUERY.getQuery();
+    public static final String WORKLOAD_QUERY = DataSourceQueries.PromQLQuery.WORKLOAD_INFO_QUERY.getQuery();
+    public static final String CONTAINER_QUERY = DataSourceQueries.PromQLQuery.CONTAINER_INFO_QUERY.getQuery();
+
+}
