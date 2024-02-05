d76b0290c27c10e72be0c3c5389cd9eba4ee6815
Shreya <shbirada@redhat.com>
2024-02-05 11:24:59 +0530

    Add exceptions to handle data source details

---
+++ src/main/java/com/autotune/common/exceptions/DataSourceDetailsInfoCreationException.java
@@ -0,0 +1,11 @@
+package com.autotune.common.exceptions;
+
+public class DataSourceDetailsInfoCreationException extends Exception {
+
+    public DataSourceDetailsInfoCreationException() {
+    }
+
+    public DataSourceDetailsInfoCreationException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/autotune/common/exceptions/DataSourceDetailsMissingRequiredField.java
@@ -0,0 +1,10 @@
+package com.autotune.common.exceptions;
+
+public class DataSourceDetailsMissingRequiredField extends Exception {
+
+    public DataSourceDetailsMissingRequiredField() {
+    }
+    public DataSourceDetailsMissingRequiredField(String message) {
+        super(message);
+    }
+}
