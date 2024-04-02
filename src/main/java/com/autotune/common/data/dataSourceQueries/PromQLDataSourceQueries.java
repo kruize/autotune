00b2dbedb24ee25b855eb010fe861461ad70c870
Shreya <shbirada@redhat.com>
2024-04-02 13:31:22 +0530

    Remove dummy cluster query

--- src/main/java/com/autotune/common/data/dataSourceQueries/PromQLDataSourceQueries.java
+++ src/main/java/com/autotune/common/data/dataSourceQueries/PromQLDataSourceQueries.java
@@ -5,7 +5,6 @@ package com.autotune.common.data.dataSourceQueries;
  * It utilizes the queries defined in the DataSourceQueries class.
  */
 public class PromQLDataSourceQueries {
-    public static final String CLUSTER_QUERY = DataSourceQueries.PromQLQuery.CLUSTER_QUERY.getQuery();
     public static final String NAMESPACE_QUERY = DataSourceQueries.PromQLQuery.NAMESPACE_QUERY.getQuery();
     public static final String WORKLOAD_QUERY = DataSourceQueries.PromQLQuery.WORKLOAD_INFO_QUERY.getQuery();
     public static final String CONTAINER_QUERY = DataSourceQueries.PromQLQuery.CONTAINER_INFO_QUERY.getQuery();
