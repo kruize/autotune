7a035a8d8522d3abefb50fb6b1e70b85377ae28e
Shreya <shbirada@redhat.com>
2024-06-06 19:42:39 +0530

    Update logs to include experiment_name and interval_end_time

--- src/main/java/com/autotune/utils/KruizeConstants.java
+++ src/main/java/com/autotune/utils/KruizeConstants.java
@@ -44,6 +44,7 @@ public class KruizeConstants {
         public static final String UPDATE_RECOMMENDATIONS_FAILURE = "UpdateRecommendations API failure response, experiment_name: %s and intervalEndTimeStr : %s";
         public static final String UPDATE_RECOMMENDATIONS_VALIDATION_FAILURE = "Validation failed: %s for experiment_name: %s and interval_end_time: %s";
         public static final String UPDATE_RECOMMENDATIONS_RESPONSE = "Update Recommendation API response: %s";
+        public static final String UPDATE_RECOMMENDATIONS_FAILURE_MSG = "UpdateRecommendations API failed for experiment_name: %s and intervalEndTimeStr : %s due to %s";
     }
 
 
