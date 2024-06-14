9653900dec92ae884cae98a81a514d1b328605bd
Saad Khan <saakhan@redhat.com>
2024-06-14 11:37:42 +0530

    update copyright year in prometheus_on_kind file
    Signed-off-by: Saad Khan <saakhan@redhat.com>

--- scripts/prometheus_on_kind.sh
+++ scripts/prometheus_on_kind.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2020, 2021 Red Hat, IBM Corporation and others.
+# Copyright (c) 2024 Red Hat, IBM Corporation and others.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
