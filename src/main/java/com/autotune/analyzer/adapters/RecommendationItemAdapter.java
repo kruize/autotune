b291d5f990749ba547e81a723e16052e5747630f
bharathappali <abharath@redhat.com>
2024-07-17 01:25:08 +0530

    Add deserializer
    Signed-off-by: bharathappali <abharath@redhat.com>

--- src/main/java/com/autotune/analyzer/adapters/RecommendationItemAdapter.java
+++ src/main/java/com/autotune/analyzer/adapters/RecommendationItemAdapter.java
@@ -1,15 +1,25 @@
 package com.autotune.analyzer.adapters;
 
 import com.autotune.analyzer.utils.AnalyzerConstants;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonSerializationContext;
-import com.google.gson.JsonSerializer;
+import com.google.gson.*;
 
 import java.lang.reflect.Type;
 
-public class RecommendationItemAdapter implements JsonSerializer<AnalyzerConstants.RecommendationItem> {
+public class RecommendationItemAdapter implements JsonSerializer<AnalyzerConstants.RecommendationItem>, JsonDeserializer<AnalyzerConstants.RecommendationItem> {
     @Override
     public JsonElement serialize(AnalyzerConstants.RecommendationItem recommendationItem, Type type, JsonSerializationContext jsonSerializationContext) {
         return jsonSerializationContext.serialize(recommendationItem.toString());
+    }
+
+
+    @Override
+    public AnalyzerConstants.RecommendationItem deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+        String value = jsonElement.getAsString();
+        for (AnalyzerConstants.RecommendationItem item : AnalyzerConstants.RecommendationItem.values()) {
+            if (item.toString().equals(value)) {
+                return item;
+            }
+        }
+        throw new JsonParseException("Unknown element " + value);
     }
 }
