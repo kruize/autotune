6b3ee7c71694414a1554ddecf59d37ba3fb690f1
Krishna Harsha Voora <krvoora@redhat.com>
2024-08-28 18:12:38 +0530

    Removes minor nits
    Signed-off-by: Krishna Harsha Voora <krvoora@redhat.com>

--- scripts/aks-helpers.sh
+++ scripts/aks-helpers.sh
@@ -73,7 +73,6 @@ function aks_deploy() {
 
 	# Replace autotune docker image in deployment yaml
 	sed -e "s|{{ AUTOTUNE_IMAGE }}|${AUTOTUNE_DOCKER_IMAGE}|" ${AUTOTUNE_DEPLOY_MANIFEST_TEMPLATE} > ${AUTOTUNE_DEPLOY_MANIFEST}
-	#sed -i "s|{{ HPO_IMAGE }}|${HPO_DOCKER_IMAGE}|" ${AUTOTUNE_DEPLOY_MANIFEST}
 	sed -i'' -e "s|{{ HPO_IMAGE }}|${HPO_DOCKER_IMAGE}|" "${AUTOTUNE_DEPLOY_MANIFEST}"
 
 	${kubectl_cmd} apply -f ${AUTOTUNE_DEPLOY_MANIFEST}
