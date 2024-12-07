ab45c34fb3650ee0b51f9e09d6adae2b9f51d952
msvinaykumar <vinakuma@redhat.com>
2024-12-07 08:23:26 +0530

    truning Local=true for RM in test script
    Signed-off-by: msvinaykumar <vinakuma@redhat.com>

--- tests/scripts/common/common_functions.sh
+++ tests/scripts/common/common_functions.sh
@@ -1904,7 +1904,7 @@ function kruize_local_patch() {
 }
 
 #
-# "local" flag is turned off for RM.
+# "isROSEnabled" flag is turned on for RM.
 # Restores kruize default cpu/memory resources, PV storage for openshift
 #
 function kruize_remote_patch() {
@@ -1914,13 +1914,15 @@ function kruize_remote_patch() {
 
 
   if [ ${cluster_type} == "minikube" ]; then
-    sed -i 's/"local": "true"/"local": "false"/' ${KRUIZE_CRC_DEPLOY_MANIFEST_MINIKUBE}
-    sed -i '/"local": "false"/a \ \ \ \ "isROSEnabled": "true",' ${KRUIZE_CRC_DEPLOY_MANIFEST_MINIKUBE}
+    sed -i 's/"isROSEnabled": "false"/"isROSEnabled": "true"/' ${KRUIZE_CRC_DEPLOY_MANIFEST_MINIKUBE}
+    #sed -i 's/"local": "true"/"local": "false"/' ${KRUIZE_CRC_DEPLOY_MANIFEST_MINIKUBE}
+    #sed -i '/"local": "true"/a \ \ \ \ "isROSEnabled": "true",' ${KRUIZE_CRC_DEPLOY_MANIFEST_MINIKUBE}
   elif [ ${cluster_type} == "openshift" ]; then
-    sed -i 's/"local": "true"/"local": "false"/' ${KRUIZE_CRC_DEPLOY_MANIFEST_OPENSHIFT}
+    sed -i 's/"isROSEnabled": "false"/"isROSEnabled": "true"/' ${KRUIZE_CRC_DEPLOY_MANIFEST_OPENSHIFT}
+    #sed -i 's/"local": "true"/"local": "false"/' ${KRUIZE_CRC_DEPLOY_MANIFEST_OPENSHIFT}
     sed -i 's/\([[:space:]]*\)\(storage:\)[[:space:]]*[0-9]\+Mi/\1\2 1Gi/' ${KRUIZE_CRC_DEPLOY_MANIFEST_OPENSHIFT}
     sed -i 's/\([[:space:]]*\)\(memory:\)[[:space:]]*".*"/\1\2 "2Gi"/; s/\([[:space:]]*\)\(cpu:\)[[:space:]]*".*"/\1\2 "2"/' ${KRUIZE_CRC_DEPLOY_MANIFEST_OPENSHIFT}
-    sed -i '/"local": "false"/a \ \ \ \ "isROSEnabled": "true",' ${KRUIZE_CRC_DEPLOY_MANIFEST_OPENSHIFT}
+    #sed -i '/"local": "true"/a \ \ \ \ "isROSEnabled": "true",' ${KRUIZE_CRC_DEPLOY_MANIFEST_OPENSHIFT}
   fi
 }
 
--- tests/scripts/remote_monitoring_tests/remote_monitoring_tests.sh
+++ tests/scripts/remote_monitoring_tests/remote_monitoring_tests.sh
@@ -61,7 +61,7 @@ function remote_monitoring_tests() {
 	if [ ${skip_setup} -eq 0 ]; then
 		echo "Setting up kruize..." | tee -a ${LOG}
 		echo "${KRUIZE_SETUP_LOG}"
-		echo "setting local=false"
+		echo "setting isROSEnabled=false"
     kruize_remote_patch
 		setup "${KRUIZE_POD_LOG}" >> ${KRUIZE_SETUP_LOG} 2>&1
 	        echo "Setting up kruize...Done" | tee -a ${LOG}
