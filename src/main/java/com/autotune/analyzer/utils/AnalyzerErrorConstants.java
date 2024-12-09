2704ce95a2f0f8fe3164f64612e5e04b687e47a5
Shekhar Saxena <shekhar.happy77@gmail.com>
2024-12-10 02:15:13 +0530

    adding support for updater service
    Signed-off-by: Shekhar Saxena <shekhar.happy77@gmail.com>

--- src/main/java/com/autotune/analyzer/utils/AnalyzerErrorConstants.java
+++ src/main/java/com/autotune/analyzer/utils/AnalyzerErrorConstants.java
@@ -292,6 +292,7 @@ public class AnalyzerErrorConstants {
 
         }
 
+        public static final String UPDTAER_SERVICE_START_ERROR = "Error occurred while initializing RecommendationUpdaterService.";
         public static final String UNSUPPORTED_UPDATER_TYPE = "Updater type %s is not supported.";
         public static final String GENERATE_RECOMMNEDATION_FAILED = "Failed to generate recommendations for experiment: {}";
         public static final String UPDATER_NOT_INSTALLED = "Updater is not installed.";
