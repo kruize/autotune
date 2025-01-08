d0617380cedd434a59dd89f042a0efba1522504f
bhanvimenghani <menghanibhanvi@gmail.com>
2025-01-08 14:42:33 +0530

    adds Model and Term setting class

---
+++ src/main/java/com/autotune/analyzer/kruizeObject/ModelSettings.java
@@ -0,0 +1,24 @@
+package com.autotune.analyzer.kruizeObject;
+
+import java.util.List;
+
+public class ModelSettings {
+
+    private List<String> models;
+
+    public List<String> getModels() {
+        return models;
+    }
+
+    public void setModels(List<String> models) {
+        this.models = models;
+    }
+
+    @Override
+    public String toString() {
+        return "ModelSettings{" +
+                "models=" + models +
+                '}';
+    }
+
+}
