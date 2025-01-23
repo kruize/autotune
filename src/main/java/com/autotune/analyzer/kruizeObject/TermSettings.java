0758ad86414707d92b2ba926a22f0b5b5c8ac336
bhanvimenghani <menghanibhanvi@gmail.com>
2025-01-23 17:02:26 +0530

    adds single custom term settings, removes extra commented code

--- src/main/java/com/autotune/analyzer/kruizeObject/TermSettings.java
+++ src/main/java/com/autotune/analyzer/kruizeObject/TermSettings.java
@@ -7,8 +7,8 @@ import java.util.Map;
 
 public class TermSettings {
     private List<String> terms;
-    @SerializedName("term_duration_in_days")
-    private Map<String, Integer> termDurations;
+    @SerializedName("term_details")
+    private Map<String, TermDetails> termDetails;
 
     public TermSettings() {}
 
@@ -20,19 +20,19 @@ public class TermSettings {
         this.terms = terms;
     }
 
-    public Map<String, Integer> getTermDurations() {
-        return termDurations;
+    public Map<String, TermDetails> getTermDetails() {
+        return termDetails;
     }
 
-    public void setTermDurations(Map<String, Integer> termDurations) {
-        this.termDurations = termDurations;
+    public void setTermDetails(Map<String, TermDetails> termDetails) {
+        this.termDetails = termDetails;
     }
 
     @Override
     public String toString() {
         return "TermSettings{" +
                 "terms=" + terms + 
-                ", termDurations=" + termDurations +
+                ", termDetails=" + termDetails +
                 '}';
     }
 }
