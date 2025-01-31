0bfddeacf72155e5d27b527aabcfafcc3d46e89e
msvinaykumar <vinakuma@redhat.com>
2025-01-31 15:53:39 +0530

    Datasource Metaata change and Metadata inclusion
    Signed-off-by: msvinaykumar <vinakuma@redhat.com>

--- src/main/java/com/autotune/common/data/dataSourceMetadata/DataSourceWorkload.java
+++ src/main/java/com/autotune/common/data/dataSourceMetadata/DataSourceWorkload.java
@@ -1,6 +1,7 @@
 package com.autotune.common.data.dataSourceMetadata;
 
 import com.autotune.utils.KruizeConstants;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.gson.annotations.SerializedName;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,36 +15,41 @@ import java.util.HashMap;
 public class DataSourceWorkload {
     private static final Logger LOGGER = LoggerFactory.getLogger(DataSourceWorkload.class);
     @SerializedName(KruizeConstants.DataSourceConstants.DataSourceMetadataInfoJSONKeys.WORKLOAD_NAME)
+    @JsonProperty(KruizeConstants.DataSourceConstants.DataSourceMetadataInfoJSONKeys.WORKLOAD_NAME)
     private String workloadName;
     @SerializedName(KruizeConstants.DataSourceConstants.DataSourceMetadataInfoJSONKeys.WORKLOAD_TYPE)
+    @JsonProperty(KruizeConstants.DataSourceConstants.DataSourceMetadataInfoJSONKeys.WORKLOAD_TYPE)
     private String workloadType;
 
     /**
      * Key: Container name
      * Value: Associated DataSourceContainer object
      */
-    @SerializedName(KruizeConstants.DataSourceConstants.DataSourceMetadataInfoJSONKeys.CONTAINERS)
-    private HashMap<String, DataSourceContainer> containerHashMap;
+    private HashMap<String, DataSourceContainer> containers;
 
-    public DataSourceWorkload(String workloadName, String workloadType, HashMap<String, DataSourceContainer> containerHashMap) {
+    public DataSourceWorkload(String workloadName, String workloadType, HashMap<String, DataSourceContainer> containers) {
         this.workloadName = workloadName;
         this.workloadType = workloadType;
-        this.containerHashMap = containerHashMap;
+        this.containers = containers;
     }
 
-    public String getDataSourceWorkloadName() {return workloadName;}
-
-    public String getDataSourceWorkloadType() {return workloadType;}
-
-    public HashMap<String, DataSourceContainer> getDataSourceContainerHashMap() {
-        return containerHashMap;
+    public String getWorkloadName() {
+        return workloadName;
     }
 
-    public void setDataSourceContainerHashMap(HashMap<String, DataSourceContainer> containerHashMap) {
+    public String getWorkloadType() {
+        return workloadType;
+    }
+
+    public HashMap<String, DataSourceContainer> getContainers() {
+        return containers;
+    }
+
+    public void setContainers(HashMap<String, DataSourceContainer> containerHashMap) {
         if (containerHashMap == null) {
             LOGGER.debug(KruizeConstants.DataSourceConstants.DataSourceMetadataErrorMsgs.SET_CONTAINER_MAP_ERROR + "{}", workloadName);
         }
-        this.containerHashMap = containerHashMap;
+        this.containers = containerHashMap;
     }
 
     @Override
@@ -51,7 +57,7 @@ public class DataSourceWorkload {
         return "DataSourceWorkload{" +
                 "workload_name='" + workloadName + '\'' +
                 ", workload_type='" + workloadType + '\'' +
-                ", conatiners=" + containerHashMap +
+                ", conatiners=" + containers +
                 '}';
     }
 }
