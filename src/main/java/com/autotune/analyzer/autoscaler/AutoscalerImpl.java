e5a758e1889617b24cc881db05b3903c0b4df7e5
bhanvimenghani <menghanibhanvi@gmail.com>
2025-02-20 16:45:10 +0530

    create exp test changes

--- src/main/java/com/autotune/analyzer/autoscaler/AutoscalerImpl.java
+++ src/main/java/com/autotune/analyzer/autoscaler/AutoscalerImpl.java
@@ -17,9 +17,7 @@
 package com.autotune.analyzer.autoscaler;
 
 import com.autotune.analyzer.autoscaler.vpa.VpaAutoscalerImpl;
-import com.autotune.analyzer.exceptions.ApplyRecommendationsError;
-import com.autotune.analyzer.exceptions.FetchMetricsError;
-import com.autotune.analyzer.exceptions.InvalidRecommendationUpdaterType;
+import com.autotune.analyzer.exceptions.*;
 import com.autotune.analyzer.kruizeObject.KruizeObject;
 import com.autotune.analyzer.recommendations.engine.RecommendationEngine;
 import com.autotune.analyzer.utils.AnalyzerConstants;
@@ -84,7 +82,12 @@ public class AutoscalerImpl implements Autoscaler {
             } else {
                 throw new Exception(validationMessage);
             }
-        } catch (Exception | FetchMetricsError e) {
+        } catch (Exception | InvalidModelException | InvalidTermException e) {
+            LOGGER.error(e.getMessage());
+//            sendErrorResponse(response, new Exception(e), HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
+            LOGGER.debug(e.getMessage());
+            return null;
+        } catch ( FetchMetricsError e) {
             LOGGER.error(AnalyzerErrorConstants.RecommendationUpdaterErrors.GENERATE_RECOMMNEDATION_FAILED, experimentName);
             LOGGER.debug(e.getMessage());
             return null;
