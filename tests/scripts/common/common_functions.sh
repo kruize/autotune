58520fbc17247921e93c68bdd8ad3fc14749d80b
Shreya <shbirada@redhat.com>
2025-03-10 13:36:35 +0530

    Add new marker 'test_bulk_api_ros' for bulk-api-ros testing

--- tests/scripts/common/common_functions.sh
+++ tests/scripts/common/common_functions.sh
@@ -1888,29 +1888,37 @@ function create_metric_profile() {
 }
 
 #
-# "local" flag is turned off by default for now. This needs to be set to true.
-#
-function kruize_local_patch() {
-	CRC_DIR="./manifests/crc/default-db-included-installation"
-	KRUIZE_CRC_DEPLOY_MANIFEST_OPENSHIFT="${CRC_DIR}/openshift/kruize-crc-openshift.yaml"
-	KRUIZE_CRC_DEPLOY_MANIFEST_MINIKUBE="${CRC_DIR}/minikube/kruize-crc-minikube.yaml"
-}
-
-#
+# This function will be used to test bulk API for ROS use case
 # "isROSEnabled" flag is turned on for RM.
-# Restores kruize default cpu/memory resources, PV storage for openshift
 # Adds `metadataProfileFilePath` and `metricProfileFilePath` under kruizeconfigjson to mount the respective file paths
 #
-function kruize_remote_patch() {
+function kruize_local_ros_patch() {
 	CRC_DIR="./manifests/crc/default-db-included-installation"
 	KRUIZE_CRC_DEPLOY_MANIFEST_OPENSHIFT="${CRC_DIR}/openshift/kruize-crc-openshift.yaml"
 	KRUIZE_CRC_DEPLOY_MANIFEST_MINIKUBE="${CRC_DIR}/minikube/kruize-crc-minikube.yaml"
 
+	if [ ${cluster_type} == "minikube" ]; then
+      if grep -q '"isROSEnabled": "false"' ${KRUIZE_CRC_DEPLOY_MANIFEST_MINIKUBE}; then
+        echo "match found"
+        sed -i 's/"isROSEnabled": "false"/"isROSEnabled": "true"/' ${KRUIZE_CRC_DEPLOY_MANIFEST_MINIKUBE}
 
-  if [ ${cluster_type} == "minikube" ]; then
-    if grep -q '"isROSEnabled": "false"' ${KRUIZE_CRC_DEPLOY_MANIFEST_MINIKUBE}; then
-      echo "match found"
-      sed -i 's/"isROSEnabled": "false"/"isROSEnabled": "true"/' ${KRUIZE_CRC_DEPLOY_MANIFEST_MINIKUBE}
+        # Use awk to find the 'kruizeconfigjson' block and insert 'metricProfileFilePath' and 'metadataProfileFilePath' before "hibernate"
+        awk '
+        /kruizeconfigjson: \|/ {in_config=1}
+        in_config && /"hibernate":/ {
+            print "      \"metricProfileFilePath\": \"/home/autotune/app/manifests/autotune/performance-profiles/resource_optimization_local_monitoring.json\",";
+            print "      \"metadataProfileFilePath\": \"/home/autotune/app/manifests/autotune/metadata-profiles/bulk_cluster_metadata_local_monitoring.json\",";
+            print
+            next
+        }
+        {print}
+        ' "${KRUIZE_CRC_DEPLOY_MANIFEST_MINIKUBE}" > temp.yaml && mv temp.yaml "${KRUIZE_CRC_DEPLOY_MANIFEST_MINIKUBE}"
+      else
+        echo "Error: Match not found" >&2
+        exit 1
+      fi
+    elif [ ${cluster_type} == "openshift" ]; then
+      sed -i 's/"isROSEnabled": "false"/"isROSEnabled": "true"/' ${KRUIZE_CRC_DEPLOY_MANIFEST_OPENSHIFT}
 
       # Use awk to find the 'kruizeconfigjson' block and insert 'metricProfileFilePath' and 'metadataProfileFilePath' before "hibernate"
       awk '
@@ -1922,7 +1930,24 @@ function kruize_remote_patch() {
           next
       }
       {print}
-      ' "${KRUIZE_CRC_DEPLOY_MANIFEST_MINIKUBE}" > temp.yaml && mv temp.yaml "${KRUIZE_CRC_DEPLOY_MANIFEST_MINIKUBE}"
+      ' "${KRUIZE_CRC_DEPLOY_MANIFEST_OPENSHIFT}" > temp.yaml && mv temp.yaml "${KRUIZE_CRC_DEPLOY_MANIFEST_OPENSHIFT}"
+    fi
+}
+
+#
+# "isROSEnabled" flag is turned on for RM.
+# Restores kruize default cpu/memory resources, PV storage for openshift
+#
+function kruize_remote_patch() {
+	CRC_DIR="./manifests/crc/default-db-included-installation"
+	KRUIZE_CRC_DEPLOY_MANIFEST_OPENSHIFT="${CRC_DIR}/openshift/kruize-crc-openshift.yaml"
+	KRUIZE_CRC_DEPLOY_MANIFEST_MINIKUBE="${CRC_DIR}/minikube/kruize-crc-minikube.yaml"
+
+
+  if [ ${cluster_type} == "minikube" ]; then
+    if grep -q '"isROSEnabled": "false"' ${KRUIZE_CRC_DEPLOY_MANIFEST_MINIKUBE}; then
+      echo "match found"
+      sed -i 's/"isROSEnabled": "false"/"isROSEnabled": "true"/' ${KRUIZE_CRC_DEPLOY_MANIFEST_MINIKUBE}
     else
       echo "Error: Match not found" >&2
       exit 1
@@ -1931,18 +1956,6 @@ function kruize_remote_patch() {
     sed -i 's/"isROSEnabled": "false"/"isROSEnabled": "true"/' ${KRUIZE_CRC_DEPLOY_MANIFEST_OPENSHIFT}
     sed -i 's/\([[:space:]]*\)\(storage:\)[[:space:]]*[0-9]\+Mi/\1\2 1Gi/' ${KRUIZE_CRC_DEPLOY_MANIFEST_OPENSHIFT}
     sed -i 's/\([[:space:]]*\)\(memory:\)[[:space:]]*".*"/\1\2 "2Gi"/; s/\([[:space:]]*\)\(cpu:\)[[:space:]]*".*"/\1\2 "2"/' ${KRUIZE_CRC_DEPLOY_MANIFEST_OPENSHIFT}
-
-    # Use awk to find the 'kruizeconfigjson' block and insert 'metricProfileFilePath' and 'metadataProfileFilePath' before "hibernate"
-    awk '
-    /kruizeconfigjson: \|/ {in_config=1}
-    in_config && /"hibernate":/ {
-        print "      \"metricProfileFilePath\": \"/home/autotune/app/manifests/autotune/performance-profiles/resource_optimization_local_monitoring.json\",";
-        print "      \"metadataProfileFilePath\": \"/home/autotune/app/manifests/autotune/metadata-profiles/bulk_cluster_metadata_local_monitoring.json\",";
-        print
-        next
-    }
-    {print}
-    ' "${KRUIZE_CRC_DEPLOY_MANIFEST_OPENSHIFT}" > temp.yaml && mv temp.yaml "${KRUIZE_CRC_DEPLOY_MANIFEST_OPENSHIFT}"
   fi
 }
 
--- tests/scripts/local_monitoring_tests/rest_apis/test_bulkAPI.py
+++ tests/scripts/local_monitoring_tests/rest_apis/test_bulkAPI.py
@@ -22,12 +22,15 @@ sys.path.append("../../")
 from helpers.fixtures import *
 from helpers.kruize import *
 from helpers.utils import *
+from helpers.list_metric_profiles_validate import *
+from helpers.list_metric_profiles_without_parameters_schema import *
 
 # Set up logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
 
+@pytest.mark.test_bulk_api_ros
 @pytest.mark.sanity
 @pytest.mark.parametrize("bulk_request_payload, expected_job_id_present", [
     ({}, True),  # Test with an empty payload to check if a job_id is created.
@@ -53,9 +56,18 @@ def test_bulk_post_request(cluster_type, bulk_request_payload, expected_job_id_p
     form_kruize_url(cluster_type)
     URL = get_kruize_url()
 
+    # list and validate default metric profile
+    response = list_metric_profiles(verbose="true", logging=False)
+    metric_profile_json = response.json()
+
+    assert response.status_code == SUCCESS_200_STATUS_CODE
+
+    errorMsg = validate_list_metric_profiles_json(metric_profile_json, list_metric_profiles_schema)
+    assert errorMsg == ""
+
     with caplog.at_level(logging.INFO):
         # Log request payload and curl command for POST request
-        response = post_bulk_api(bulk_request_payload)
+        response = post_bulk_api(bulk_request_payload, logging)
 
         # Check if job_id is present in the response
         job_id_present = "job_id" in response.json() and isinstance(response.json()["job_id"], str)
