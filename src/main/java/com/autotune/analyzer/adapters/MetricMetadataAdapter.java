30901e45c303f7e8ad3fe04425955bb13a77cad4
bharathappali <abharath@redhat.com>
2025-06-17 17:00:54 +0530

    Add license and javadoc
    Signed-off-by: bharathappali <abharath@redhat.com>

--- src/main/java/com/autotune/analyzer/adapters/MetricMetadataAdapter.java
+++ src/main/java/com/autotune/analyzer/adapters/MetricMetadataAdapter.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Red Hat, IBM Corporation and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 package com.autotune.analyzer.adapters;
 
 import com.autotune.analyzer.utils.AnalyzerConstants;
@@ -12,9 +27,40 @@ import com.autotune.utils.KruizeConstants;
 import java.io.IOException;
 
 /**
- * This adapter actually specifies the GSON to identify the type of implementation of MetricMetadata
- * to serialize or deserialize
+ * A custom Gson {@link TypeAdapter} implementation for serializing and deserializing
+ * {@link MetricMetadata} objects. This adapter currently supports handling metadata
+ * for accelerator devices (e.g., GPUs) via the {@link AcceleratorMetricMetadata} class.
+ *
+ * <p><b>Current Implementation:</b></p>
+ * <ul>
+ *     <li>During serialization:
+ *         <ul>
+ *             <li>Writes the `type` field to identify the metadata type.</li>
+ *             <li>For {@link AcceleratorMetricMetadata}, writes fields such as model name,
+ *                 profile name (MIG profile), and node name.</li>
+ *         </ul>
+ *     </li>
+ *     <li>During deserialization:
+ *         <ul>
+ *             <li>Reads the `type` field and maps it to the appropriate implementation.</li>
+ *             <li>If the `type` is <code>null</code> or equals "accelerator" (case-insensitive),
+ *                 an {@link AcceleratorMetricMetadata} instance is created.</li>
+ *             <li>Unknown fields are safely skipped.</li>
+ *         </ul>
+ *     </li>
+ * </ul>
+ *
+ * <p><b>Future Considerations:</b></p>
+ * <ul>
+ *     <li>Currently, the adapter assumes only accelerator metadata exists.</li>
+ *     <li>In the future, support for additional metric metadata types should enforce a non-null `type`
+ *         field for proper deserialization.</li>
+ *     <li>This class should be extended to handle multiple types of {@link MetricMetadata}
+ *         by mapping the `type` field to the appropriate class.</li>
+ * </ul>
+ *
  */
+
 public class MetricMetadataAdapter extends TypeAdapter<MetricMetadata> {
 
     @Override
