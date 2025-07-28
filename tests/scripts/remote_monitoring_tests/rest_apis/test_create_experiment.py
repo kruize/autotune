83d8863f288d14d7d5945bf68e133750c7ca51ee
Saad Khan <saakhan@ibm.com>
2025-07-28 23:46:35 +0530

    add sanity test for new DB columns
    Signed-off-by: Saad Khan <saakhan@ibm.com>

--- tests/scripts/remote_monitoring_tests/rest_apis/test_create_experiment.py
+++ tests/scripts/remote_monitoring_tests/rest_apis/test_create_experiment.py
@@ -677,3 +677,139 @@ def test_create_multiple_namespace_exp(cluster_type):
 
     response = delete_experiment(input_json_file, rm=True)
     print("delete exp = ", response.status_code)
+
+
+@pytest.mark.sanity
+def test_create_exp_exptype_dates_columns(cluster_type):
+    """
+    Test Description: This test validates the response status code of createExperiment API by passing a
+    valid input for the json
+    """
+    input_json_file = "../json_files/create_exp.json"
+    form_kruize_url(cluster_type)
+
+    response = delete_experiment(input_json_file)
+    print("delete exp = ", response.status_code)
+
+    # Create experiment using the specified json
+    response = create_experiment(input_json_file)
+
+    data = response.json()
+    print(data['message'])
+
+    assert response.status_code == SUCCESS_STATUS_CODE
+    assert data['status'] == SUCCESS_STATUS
+    assert data['message'] == CREATE_EXP_SUCCESS_MSG
+
+    # Fetch listExperiments
+    results = "false"
+    recommendations = "false"
+    latest = "false"
+    experiment_name = None
+    response = list_experiments(results, recommendations, latest, experiment_name, rm=True)
+    if response.status_code == SUCCESS_200_STATUS_CODE:
+        list_exp_before = response.json()
+        # Validate timestamp fields
+        assert "experiment_type" in list_exp_before[0]
+        assert "creation_date" in list_exp_before[0]
+        assert "update_date" in list_exp_before[0]
+        assert list_exp_before[0]["creation_date"] is not None
+        assert list_exp_before[0]["update_date"] is not None
+        assert list_exp_before[0]["experiment_type"] == CONTAINER_EXPERIMENT_TYPE
+
+        old_update_date = list_exp_before[0]["update_date"]
+        # induce a delay os 2 secs, then call the updateResults API to check the update timestamp
+        time.sleep(2)
+
+        # call updateResults
+        result_json_file = "../json_files/update_results.json"
+        response = update_results(result_json_file)
+
+        data = response.json()
+        assert response.status_code == SUCCESS_STATUS_CODE
+        assert data['status'] == SUCCESS_STATUS
+        assert data['message'] == UPDATE_RESULTS_SUCCESS_MSG
+
+        # call list exp again to check the updated date
+        response = list_experiments(results, recommendations, latest, experiment_name, rm=True)
+        if response.status_code == SUCCESS_200_STATUS_CODE:
+            list_exp_after = response.json()
+            new_update_date = list_exp_after[0]["update_date"]
+            assert new_update_date != old_update_date, f"update_date did not change: {new_update_date} == {old_update_date}"
+        else:
+            print(f"listExperiments failed in the second call!")
+            sys.exit(1)
+    else:
+        print(f"listExperiments failed!")
+        sys.exit(1)
+
+    response = delete_experiment(input_json_file)
+    print("delete exp = ", response.status_code)
+
+
+@pytest.mark.sanity
+def test_create_exp_exptype_dates_columns(cluster_type):
+    """
+    Test Description: This test validates the new columns in the DB(namely experiment_type, creation_date, update_date) and its values by passing a
+    valid input json
+    """
+    input_json_file = "../json_files/create_exp.json"
+    form_kruize_url(cluster_type)
+
+    response = delete_experiment(input_json_file)
+    print("delete exp = ", response.status_code)
+
+    # Create experiment using the specified json
+    response = create_experiment(input_json_file)
+
+    data = response.json()
+    print(data['message'])
+
+    assert response.status_code == SUCCESS_STATUS_CODE
+    assert data['status'] == SUCCESS_STATUS
+    assert data['message'] == CREATE_EXP_SUCCESS_MSG
+
+    # Fetch listExperiments
+    results = "false"
+    recommendations = "false"
+    latest = "false"
+    experiment_name = None
+    response = list_experiments(results, recommendations, latest, experiment_name, rm=True)
+    if response.status_code == SUCCESS_200_STATUS_CODE:
+        list_exp_before = response.json()
+        # Validate timestamp fields
+        assert "experiment_type" in list_exp_before[0]
+        assert "creation_date" in list_exp_before[0]
+        assert "update_date" in list_exp_before[0]
+        assert list_exp_before[0]["creation_date"] is not None
+        assert list_exp_before[0]["update_date"] is not None
+        assert list_exp_before[0]["experiment_type"] == CONTAINER_EXPERIMENT_TYPE
+
+        old_update_date = list_exp_before[0]["update_date"]
+        # induce a delay os 2 secs, then call the updateResults API to check the update timestamp
+        time.sleep(2)
+
+        # call updateResults
+        result_json_file = "../json_files/update_results.json"
+        response = update_results(result_json_file)
+
+        data = response.json()
+        assert response.status_code == SUCCESS_STATUS_CODE
+        assert data['status'] == SUCCESS_STATUS
+        assert data['message'] == UPDATE_RESULTS_SUCCESS_MSG
+
+        # call list exp again to check the updated date
+        response = list_experiments(results, recommendations, latest, experiment_name, rm=True)
+        if response.status_code == SUCCESS_200_STATUS_CODE:
+            list_exp_after = response.json()
+            new_update_date = list_exp_after[0]["update_date"]
+            assert new_update_date != old_update_date, f"update_date did not change: {new_update_date} == {old_update_date}"
+        else:
+            print(f"listExperiments failed in the second call!")
+            sys.exit(1)
+    else:
+        print(f"listExperiments failed!")
+        sys.exit(1)
+
+    response = delete_experiment(input_json_file)
+    print("delete exp = ", response.status_code)
