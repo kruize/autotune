8c7058c9ab440694bde7dec430b7d6c7cf524dfb
bhanvimenghani <menghanibhanvi@gmail.com>
2025-10-14 13:57:50 +0530

    adding Term Defination class

---
+++ src/main/java/com/autotune/analyzer/kruizeObject/TermDefinition.java
@@ -0,0 +1,87 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Red Hat, IBM Corporation and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package com.autotune.analyzer.kruizeObject;
+
+import com.google.gson.annotations.SerializedName;
+
+public class TermDefinition {
+    private String name;
+
+    @SerializedName("duration_in_days")
+    private Integer durationInDays;
+
+    @SerializedName("duration_threshold")
+    private String durationThreshold;
+
+    @SerializedName("plots_datapoint")
+    private Integer plotsDatapoint;
+
+    @SerializedName("plots_datapoint_delta_in_days")
+    private Double plotsDatapointDeltaInDays;
+
+    public TermDefinition() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getDurationInDays() {
+        return durationInDays;
+    }
+
+    public void setDurationInDays(Integer durationInDays) {
+        this.durationInDays = durationInDays;
+    }
+
+    public String getDurationThreshold() {
+        return durationThreshold;
+    }
+
+    public void setDurationThreshold(String durationThreshold) {
+        this.durationThreshold = durationThreshold;
+    }
+
+    public Integer getPlotsDatapoint() {
+        return plotsDatapoint;
+    }
+
+    public void setPlotsDatapoint(Integer plotsDatapoint) {
+        this.plotsDatapoint = plotsDatapoint;
+    }
+
+    public Double getPlotsDatapointDeltaInDays() {
+        return plotsDatapointDeltaInDays;
+    }
+
+    public void setPlotsDatapointDeltaInDays(Double plotsDatapointDeltaInDays) {
+        this.plotsDatapointDeltaInDays = plotsDatapointDeltaInDays;
+    }
+
+    @Override
+    public String toString() {
+        return "TermDefinition{" +
+                "name='" + name +
+                ", durationInDays=" + durationInDays +
+                ", durationThreshold='" + durationThreshold +
+                ", plotsDatapoint=" + plotsDatapoint +
+                ", plotsDatapointDeltaInDays=" + plotsDatapointDeltaInDays +
+                '}';
+    }
+}
