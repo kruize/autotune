21cceede76f8024d3502f6e15e50a94fbd095403
bharathappali <abharath@redhat.com>
2025-12-11 08:50:34 +0530

    Add license header in layer utils
    Signed-off-by: bharathappali <abharath@redhat.com>

--- src/main/java/com/autotune/analyzer/kruizeLayer/utils/LayerUtils.java
+++ src/main/java/com/autotune/analyzer/kruizeLayer/utils/LayerUtils.java
@@ -21,7 +21,6 @@ import com.autotune.analyzer.kruizeLayer.presence.QueryBasedPresence;
 import com.autotune.database.service.ExperimentDBService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.util.HashMap;
 import java.util.Map;
 
