16488f6c92dfffd975714477bb70cf74bcae343d
Pinky Gupta <pinky.gupta1@ibm.com>
2026-01-14 12:37:46 +0530

    addressed sourcery comments
    Signed-off-by: Pinky Gupta <pinky.gupta1@ibm.com>

--- src/main/java/com/autotune/common/datasource/DataSourceMetadataOperator.java
+++ src/main/java/com/autotune/common/datasource/DataSourceMetadataOperator.java
@@ -15,19 +15,6 @@
  *******************************************************************************/
 package com.autotune.common.datasource;
 
-import com.autotune.analyzer.metadataProfiles.MetadataProfile;
-import com.autotune.analyzer.metadataProfiles.MetadataProfileCollection;
-import com.autotune.analyzer.utils.AnalyzerConstants;
-import com.autotune.common.data.dataSourceMetadata.*;
-import com.autotune.utils.GenericRestApiClient;
-import com.autotune.utils.KruizeConstants;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import org.json.JSONObject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.IOException;
 import java.net.URLEncoder;
 import java.security.KeyManagementException;
@@ -38,7 +25,25 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.autotune.analyzer.metadataProfiles.MetadataProfile;
+import com.autotune.analyzer.metadataProfiles.MetadataProfileCollection;
+import com.autotune.analyzer.utils.AnalyzerConstants;
 import static com.autotune.analyzer.utils.AnalyzerConstants.ServiceConstants.CHARACTER_ENCODING;
+import com.autotune.common.data.dataSourceMetadata.DataSource;
+import com.autotune.common.data.dataSourceMetadata.DataSourceContainer;
+import com.autotune.common.data.dataSourceMetadata.DataSourceMetadataHelper;
+import com.autotune.common.data.dataSourceMetadata.DataSourceMetadataInfo;
+import com.autotune.common.data.dataSourceMetadata.DataSourceNamespace;
+import com.autotune.common.data.dataSourceMetadata.DataSourceWorkload;
+import com.autotune.utils.GenericRestApiClient;
+import com.autotune.utils.KruizeConstants;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 /**
  * DataSourceMetadataOperator is an abstraction with CRUD operations to manage DataSourceMetadataInfo Object
@@ -202,12 +207,22 @@ public class DataSourceMetadataOperator {
             String filteredQuery;
             if (queryTemplate.contains("%s")) {
                 filteredQuery = String.format(queryTemplate, filter);
-            } else {
+
+            } else if (queryTemplate.contains(field + "!=\"\"")) {
                 filteredQuery = queryTemplate.replace(
                     field + "!=\"\"",
                     filter.isEmpty() ? field + "!=\"\"" : filter
                 );
+
+            } else {
+                LOGGER.warn(
+                    "No injectable filter placeholder found for field {} in queryTemplate={}",
+                    field,
+                    queryTemplate
+                );
+                filteredQuery = queryTemplate; // fallback
             }
+
             queries.put(field, filteredQuery);
         });
 
