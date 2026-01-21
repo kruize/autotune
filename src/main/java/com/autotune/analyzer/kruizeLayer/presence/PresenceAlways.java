35067e216866cc0a6291e42384cdba1e3ce13950
bhanvimenghani <menghanibhanvi@gmail.com>
2026-01-21 17:41:31 +0530

    add detection function

--- src/main/java/com/autotune/analyzer/kruizeLayer/presence/PresenceAlways.java
+++ src/main/java/com/autotune/analyzer/kruizeLayer/presence/PresenceAlways.java
@@ -23,12 +23,28 @@ import com.autotune.analyzer.utils.AnalyzerConstants.LayerConstants.PresenceType
  */
 public class PresenceAlways implements LayerPresenceDetector {
 
+    private String presence;
+
     public PresenceAlways() {}
 
 
     @Override
     public PresenceType getType() {
         return PresenceType.ALWAYS;
+    }
+
+    @Override
+    public boolean detectPresence(String namespace, String workloadName) throws Exception {
+        // Layers with ALWAYS presence type are always detected
+        return true;
+    }
+
+    public String getPresence() {
+        return presence;
+    }
+
+    public void setPresence(String presence) {
+        this.presence = presence;
     }
 
     @Override
