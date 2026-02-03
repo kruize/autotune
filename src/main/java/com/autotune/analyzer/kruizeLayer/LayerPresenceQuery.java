c4562c132269a24b615e9edc10b555bc065ec8df
bharathappali <abharath@redhat.com>
2026-02-03 12:46:31 +0530

    Add check for datasource in setters
    Signed-off-by: bharathappali <abharath@redhat.com>

--- src/main/java/com/autotune/analyzer/kruizeLayer/LayerPresenceQuery.java
+++ src/main/java/com/autotune/analyzer/kruizeLayer/LayerPresenceQuery.java
@@ -57,7 +57,8 @@ public class LayerPresenceQuery {
 	}
 
 	public void setDataSource(String dataSource) {
-		this.dataSource = dataSource;
+		if (KruizeSupportedTypes.MONITORING_AGENTS_SUPPORTED.contains(dataSource.toLowerCase()))
+			this.dataSource = dataSource;
 	}
 
 	public String getLayerPresenceKey() {
