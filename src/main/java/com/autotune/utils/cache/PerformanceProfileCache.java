49743196a59eaaf8e707c4569b434fe608cad9b4
Bhaktavatsal Reddy <bhamaram@in.ibm.com>
2026-02-03 14:14:27 +0530

    Address sourcery comments related to cache eviction
    Signed-off-by: Bhaktavatsal Reddy <bhamaram@in.ibm.com>

--- src/main/java/com/autotune/utils/cache/PerformanceProfileCache.java
+++ src/main/java/com/autotune/utils/cache/PerformanceProfileCache.java
@@ -2,10 +2,15 @@ package com.autotune.utils.cache;
 
 import com.autotune.analyzer.performanceProfiles.PerformanceProfile;
 
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class PerformanceProfileCache {
-    private static final ConcurrentHashMap<String, PerformanceProfile> performanceProfileMap = new ConcurrentHashMap<>();
+    private static final Map<String, PerformanceProfile> performanceProfileMap = new ConcurrentHashMap<>();
+
+    private PerformanceProfileCache() {
+
+    }
 
     public static boolean exists(String performanceProfileName) {
         return performanceProfileMap.containsKey(performanceProfileName);
