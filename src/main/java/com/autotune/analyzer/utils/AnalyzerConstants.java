26c0deafd918d52528483901b433772360f49ae0
Pinky Gupta <pinky.gupta1@ibm.com>
2026-09-09 19:12:11 +0530

    named placeholder implementation
    Signed-off-by: Pinky Gupta <pinky.gupta1@ibm.com>

--- src/main/java/com/autotune/analyzer/utils/AnalyzerConstants.java
+++ src/main/java/com/autotune/analyzer/utils/AnalyzerConstants.java
@@ -1168,6 +1168,8 @@ public class AnalyzerConstants {
             filter.append("workload_type!~").append("\"(?i)").append(typeString).append("\"");
         }
         
-        return filter.toString();
+        // Add leading comma for chaining with other filters in PromQL selectors
+        String result = filter.toString();
+        return result.isEmpty() ? "" : ", " + result;
     }
 }
