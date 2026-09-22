3fa3fcc76bca7347d56dc6c2c442f0158d708c51
bharathappali <abharath@redhat.com>
2026-09-22 13:40:21 +0530

    Support accelerator array in recommendation current and variation Add MultiResourceRecommendation interface, AcceleratorRecommendationItem, Gson adapter, and ExtendedConfig so current/variation can serialize CPU/memory maps plus an accelerators array without nested wrapper keys. Signed-off-by: bharathappali <abharath@redhat.com>

---
+++ src/main/java/com/autotune/analyzer/recommendations/ExtendedConfig.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2026 IBM Corporation and others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package com.autotune.analyzer.recommendations;
+
+import com.autotune.analyzer.utils.AnalyzerConstants;
+
+import java.util.Map;
+
+public class ExtendedConfig {
+    private Map<AnalyzerConstants.RecommendationItem, ResourceRecommendation> requests;
+    private Map<AnalyzerConstants.RecommendationItem, ResourceRecommendation> limits;
+
+    public Map<AnalyzerConstants.RecommendationItem, ResourceRecommendation> getRequests() {
+        return requests;
+    }
+
+    public void setRequests(Map<AnalyzerConstants.RecommendationItem, ResourceRecommendation> requests) {
+        this.requests = requests;
+    }
+
+    public Map<AnalyzerConstants.RecommendationItem, ResourceRecommendation> getLimits() {
+        return limits;
+    }
+
+    public void setLimits(Map<AnalyzerConstants.RecommendationItem, ResourceRecommendation> limits) {
+        this.limits = limits;
+    }
+}
